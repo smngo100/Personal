5e38ee4f1da1f0effbf668e2f5f24786539482a9
smngo100 <smngo100@gmail.com>
2024-10-25 21:41:19 -0700

    Create 4.1-4.2.py

---
+++ Notes/Python/Python Crash Course/4.1-4.2.py
@@ -0,0 +1,15 @@
+# 4-1. Pizzas
+fav_pizzas = ["pineapple", "mushroom", "pepperoni"]
+
+for pizza in fav_pizzas:
+    print(f"I like {pizza} pizza.")
+print(f"I love {fav_pizzas[0]} pizza a lot, it is my favorite!")
+print(f"I love {fav_pizzas[1]} pizza, it is yummy.")
+print(f"I love {fav_pizzas[2]} pizza, it is a classic.")
+print(f"I really love pizza!\n")
+
+# 4-2 Animals
+animals = ["wolf", "bear", "lion"]
+for animal in animals:
+    print(f"A {animal} would make a great pet.")
+print("Any of these animals would make a great pet!")
