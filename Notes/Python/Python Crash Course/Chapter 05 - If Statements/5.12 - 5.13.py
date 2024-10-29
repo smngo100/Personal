120ac49d276c1284508d7ad6cc7b01e37e9fa723
smngo100 <smngo100@gmail.com>
2024-10-29 11:36:37 -0700

    Create 5.12 - 5.13.py

---
+++ Notes/Python/Python Crash Course/Chapter 05 - If Statements/5.12 - 5.13.py
@@ -0,0 +1,12 @@
+# 5-12. Styling if Statemenets 
+# Already styled program
+
+# 5-13. Your Ideas 
+"""
+
+* hangman 
+* tic-tac-toe
+* search book database (refer to library search bar)
+* password generator
+
+"""
