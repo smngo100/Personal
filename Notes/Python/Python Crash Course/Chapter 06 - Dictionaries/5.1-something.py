dcdc168679ed854c5c18ae4df2e38ab46f86aed2
smngo100 <smngo100@gmail.com>
2024-10-29 11:45:56 -0700

    Create 5.1-something.py

---
+++ Notes/Python/Python Crash Course/Chapter 06 - Dictionaries/5.1-something.py
@@ -0,0 +1,2 @@
+# Dictionary: A collection of key-value pairs. Each key is connected to a value, and you can use a key to access the value associated with that key. 
+# Note: A key's value can be a number, a string, a list, or even enother dictionary. Basically, you can use any object that you can create in Python as a value in a dictionary.
