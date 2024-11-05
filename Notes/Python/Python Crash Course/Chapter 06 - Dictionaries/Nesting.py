bfaba192746b458a101045f5b5a48f9b3ae82007
smngo100 <smngo100@gmail.com>
2024-11-04 22:07:24 -0800

    Update Nesting.py

--- Notes/Python/Python Crash Course/Chapter 06 - Dictionaries/Nesting.py
+++ Notes/Python/Python Crash Course/Chapter 06 - Dictionaries/Nesting.py
@@ -9,13 +9,25 @@ for alien in aliens:
     print(alien)
 
 # A List in a Dictionary 
+# Pizza example
 pizza = {
     'crust': 'thick',
     'toppings': ['mushrooms', 'extra cheese'],
     }
-print(f"You ordered a {pizza['crust']}-crust pizza "
-      "with the following toppings:")
+print(f"You ordered a {pizza['crust']}-crust pizza with the following toppings:")
 
 for topping in pizza['toppings']:
   print(f"\t{topping}")
 
+# Languages example
+favorite_languages = {
+    'jen': ['python', 'rust'],
+    'sarah': ['c'],
+    'edward': ['rust', 'go'],
+    'phil': ['python', 'haskell'],
+    }
+
+for name, languages in favorite_languages.items():
+    print(f"\n{name.title()}'s favorite languages are:")
+    for language in languages:
+        print(f"\t{language.title()}")
