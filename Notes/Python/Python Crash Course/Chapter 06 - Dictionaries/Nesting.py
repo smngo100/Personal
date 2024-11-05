b06aad43acb8172c93f606e993f69e0938b68769
smngo100 <smngo100@gmail.com>
2024-11-04 22:19:07 -0800

    Update Nesting.py

--- Notes/Python/Python Crash Course/Chapter 06 - Dictionaries/Nesting.py
+++ Notes/Python/Python Crash Course/Chapter 06 - Dictionaries/Nesting.py
@@ -31,3 +31,28 @@ for name, languages in favorite_languages.items():
     print(f"\n{name.title()}'s favorite languages are:")
     for language in languages:
         print(f"\t{language.title()}")
+
+# A Dictionary in a Dictionary 
+users = {
+    'aeinstein': {
+        'first': 'albert',
+        'last': 'einstein',
+        'location': 'princeton',
+        },
+
+    'mcurie': {
+        'first': 'marie',
+        'last': 'curie',
+        'location': 'paris',
+        },
+
+    }
+
+for username, user_info in users.items():
+    print(f"\nUsername: {username}")
+    full_name = f"{user_info['first']} {user_info['last']}"
+    location = user_info['location']
+    
+    print(f"\tFull name: {full_name.title()}")
+    print(f"\tLocation: {location.title()}")
+
