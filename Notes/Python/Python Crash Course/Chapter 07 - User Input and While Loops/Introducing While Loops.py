b49c32cdd3ae870ad32fae33a91fb8da995abe3a
smngo100 <smngo100@gmail.com>
2024-11-05 11:41:03 -0800

    Update Introducing While Loops.py

--- Notes/Python/Python Crash Course/Chapter 07 - User Input and While Loops/Introducing While Loops.py
+++ Notes/Python/Python Crash Course/Chapter 07 - User Input and While Loops/Introducing While Loops.py
@@ -1,4 +1,3 @@
 # Recall: The for loop takes a collection of items and executes a block of code once for each item in the collection. 
 # The while loop runs as along as, or while, a certain condition is true. 
 
-# The while Loop in Action 
