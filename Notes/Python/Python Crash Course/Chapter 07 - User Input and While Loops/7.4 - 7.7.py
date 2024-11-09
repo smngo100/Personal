8620392ef548e99cc8e8586016663ac5e86eee2b
smngo100 <smngo100@gmail.com>
2024-11-08 23:58:48 -0800

    Update 7.4 - 7.7.py

--- Notes/Python/Python Crash Course/Chapter 07 - User Input and While Loops/7.4 - 7.7.py
+++ Notes/Python/Python Crash Course/Chapter 07 - User Input and While Loops/7.4 - 7.7.py
@@ -44,13 +44,27 @@ while True:
     elif 3 <= age < 12:
         ticket = "10"
         print(f"The cost of your movie ticket is ${ticket}.")
-    else:
+    elif age > 12:
         ticket = "15"
         print(f"The cost of your movie ticket is ${ticket}.")
+        break
 
 # 7-6. Three Exists
+pizza_toppings = []
+message = ""
 
+active = True
+while active:
+    message = input("\nEnter a pizza topping (or enter 'quit' to stop): ").lower()
 
-
+    if message == 'quit':
+        active = False
+    else:
+        pizza_toppings.append(message)
+        print(f"Adding {message} to pizza.")
 
 # 7-7. Infinity
+x = 1
+while x <= 5:
+    print(x)
+    # x += 1    if you omit this statement this will be an infinite loop bc x will start at 1 but never change. So the conditional test will always evaluate to True and the while loop will run forever.
