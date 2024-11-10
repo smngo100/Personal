30dd1df1c102b08506586a1622d2b2171fc9305b
smngo100 <smngo100@gmail.com>
2024-11-10 00:41:34 -0800

    Create Setting and Accessing Values.py

---
+++ Notes/Python/Python Crash Course/Chapter 06 - Dictionaries/Setting and Accessing Values.py
@@ -0,0 +1,34 @@
+# Accessing Values
+"""
+- When you use a key within square brackets with a dictionary, it retrieves the value associated with that key
+- Accessing a Value for a Key (Right Side of Assignment or in an Expression) 
+- When you use brackets on the right side of an assignment or within an expression, you are accessing the value associated with a specific key in the dictionary
+"""
+
+responses = {'Alice': 'Hello, world!'}  # Existing dictionary
+name = "Alice"  # Define a key
+
+# Accessing the value for the key "Alice"
+response = responses[name]  # This retrieves 'Hello, world!' and assigns it to `response`
+print(response)  # Output: Hello, world!
+
+
+# Setting Values 
+""" 
+- Setting a Value for a Key (Left Side of Assignment) 
+- When you use brackets on the left side of an assignment, you are setting or updating the value associated with a specific key in the dictionary
+- When you use a key within square brackets and assign a value to it, it either updates an existing key or adds a new key-value pair to the dictionary
+"""
+
+nses = {}  # Create an empty dictionary
+name = "Alice"  # Define a key
+response = "Hello, world!"  # Define a value
+
+# Setting the value for the key "Alice"
+responses[name] = response  # Now, responses = {'Alice': 'Hello, world!'}
+# responses[name] = response sets the value for the key "Alice" to "Hello, world!".
+
+
+
+
+
