554d770c1937453912d77590d9971869f003ca74
smngo100 <smngo100@gmail.com>
2024-11-10 23:50:11 -0800

    Update Introducing While Loops.py

--- Notes/Python/Python Crash Course/Chapter 07 - User Input and While Loops/Introducing While Loops.py
+++ Notes/Python/Python Crash Course/Chapter 07 - User Input and While Loops/Introducing While Loops.py
@@ -62,3 +62,9 @@ for confirmed_user in confirmed_users:
     print(confirmed_user.title())
 
 # Removing All Instance of Specific Values from a List
+pets = ['dog', 'cat', 'dog', 'goldfish', 'cat', 'rabbit', 'cat']
+print(pets)
+
+while 'cat' in pets:
+    pets.remove('cat')
+print(pets)
