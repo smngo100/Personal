c351add404cd474fbdf8e45861106c789ef289db
smngo100 <smngo100@gmail.com>
2024-11-11 23:56:59 -0800

    Update Functions.py

--- Notes/Python/Python Crash Course/Chapter 08 - Functions/Functions.py
+++ Notes/Python/Python Crash Course/Chapter 08 - Functions/Functions.py
@@ -1,1 +1,10 @@
 # Functions: named blocks of code designed to do one specific job. When you want to perform particular task that you've defined in a function, you call the function responsible for it. 
+
+def greet_user(): 
+  """Display a simple greeting."""
+  print("Hello!")
+
+greet_user() 
+
+# Keyword def informs Python that you're defining a function 
+# This is the function definition, which tells Pytthon the name of the fucntion and, if applicable, what kind of information the function needs to do its job. The parentheses hold that information.
