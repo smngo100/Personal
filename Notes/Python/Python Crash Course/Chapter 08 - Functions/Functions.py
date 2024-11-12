d682ef044c33403424143dafb3c8d3227b4e105a
smngo100 <smngo100@gmail.com>
2024-11-12 00:10:59 -0800

    Update Functions.py

--- Notes/Python/Python Crash Course/Chapter 08 - Functions/Functions.py
+++ Notes/Python/Python Crash Course/Chapter 08 - Functions/Functions.py
@@ -1,10 +1,12 @@
 # Functions: named blocks of code designed to do one specific job. When you want to perform particular task that you've defined in a function, you call the function responsible for it. 
 
+# Defining a Function 
 def greet_user(): 
-  """Display a simple greeting."""
+  """Display a simple greeting."""  # Docstring: describes what the function does. A string used to document a Python module, class, function or method.
   print("Hello!")
 
 greet_user() 
-
 # Keyword def informs Python that you're defining a function 
 # This is the function definition, which tells Pytthon the name of the fucntion and, if applicable, what kind of information the function needs to do its job. The parentheses hold that information.
+
+# Passing Information to a Function 
