137557f3e0c5b2567af6c72b0faffd254935a9fa
smngo100 <smngo100@gmail.com>
2024-11-11 23:53:44 -0800

    Create Functions.py

---
+++ Notes/Python/Python Crash Course/Chapter 08 - Functions/Functions.py
@@ -0,0 +1,1 @@
+# Functions: named blocks of code designed to do one specific job. When you want to perform particular task that you've defined in a function, you call the function responsible for it. 
