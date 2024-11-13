6bcb2018d21daf165351cdafe884af1d9fb25151
smngo100 <smngo100@gmail.com>
2024-11-13 00:19:08 -0800

    Update 4.13.py

--- Notes/Python/Python Crash Course/Chapter 04 - Working with Lists/4.13.py
+++ Notes/Python/Python Crash Course/Chapter 04 - Working with Lists/4.13.py
@@ -6,10 +6,12 @@ for food in foods:
     print(food)
 
 # Error
+# Why? Because tuples are immutable. Once you create a tuple you cannot modify it.
 """foods[0] = "steak"
 print(foods)"""
 
 # Overwriting
+# You can't modify tuples, but you can assign a new value to a variable that represents a tuple.
 foods = ('pizza', "sandwiches", "tacos", "fries", "steak")
 for food in foods:
     print(food)
