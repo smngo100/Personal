b551f43eb7c7b28e9e719991511e632f19087832
smngo100 <smngo100@gmail.com>
2024-11-14 00:09:22 -0800

    Update 8.1 - 8.2.py

--- Notes/Python/Python Crash Course/Chapter 08 - Functions/8.1 - 8.2.py
+++ Notes/Python/Python Crash Course/Chapter 08 - Functions/8.1 - 8.2.py
@@ -1,8 +1,7 @@
 # 8-1. Message
 def display_message():  # function definition
     print("I am learning about functions in this chapter.") # indented lines that follow the function definition make up the body of the funciton
-display_message()   
-# function call: tells Python to execute the code in the function
+display_message()   # function call: tells Python to execute the code in the function
 # to call function, write name of function (as done here), followed by any necessary information in parentheses
 
 # 8-2. Favorite Book
