10a419dda96b569df0914b9e1712e73b9fa3fdb2
smngo100 <smngo100@gmail.com>
2024-11-15 00:26:19 -0800

    Create 8.3 - 8.5.py

---
+++ Notes/Python/Python Crash Course/Chapter 08 - Functions/8.3 - 8.5.py
@@ -0,0 +1,25 @@
+# 8-3. T-Shirt
+def make_shirt(size, message):
+    print(f"You are size {size}. The message on the shirt says '{message.title()}'.")
+
+# Positional arguments
+make_shirt('small', 'hello, world')
+
+# Keyword arguments
+make_shirt(size = 'small', message = 'hello, world')
+
+
+# 8-4. Large Shirts
+def make_shirt(size = 'large', message = 'i love python'):
+    print(f"You are size {size}. {message.title()}.")
+make_shirt()
+make_shirt(size = 'medium')
+make_shirt(size = 'small', message = 'i love java')
+
+
+# 8-5. Cities
+def describe_city(city, country = 'the united states'):
+    print(f"{city.title()} is in {country.title()}.")
+describe_city('california')
+describe_city('san diego')
+describe_city(city = 'paris', country = 'europe')
