0035e8270f9a95eb31d93aec6be8e0466ab8413f
smngo100 <smngo100@gmail.com>
2024-11-16 00:23:09 -0800

    Update 4.10 - 4.12.py

--- Notes/Python/Python Crash Course/Chapter 04 - Working with Lists/4.10 - 4.12.py
+++ Notes/Python/Python Crash Course/Chapter 04 - Working with Lists/4.10 - 4.12.py
@@ -2,9 +2,9 @@
 
 # 4-10. Slices
 animals = ["wolf", "bear", "lion", "deer", "bobcat", "fox", "moose", "zebra"]
-print(f"The first three items in the list are: {animals[:3]}.") # animals[0:3] would give same output    # Here, it excludes the element at index 3. It will print "worlf", "bear", "lion", but not the last element "deer"
+print(f"The first three items in the list are: {animals[:3]}.")    # animals[0:3] would give same output    # Here, it excludes the element at index 3. It will print "worlf", "bear", "lion", but not the last element "deer"
 print(f"Three items from the middle of the list are: {animals[2:5]}.")
-print(f"The last three items in the list are: {animals[5:]}\n")
+print(f"The last three items in the list are: {animals[5:]}\n")    # Here, it will print at index 5 and the rest of the elements in the list           
 
 # 4-11. My Pizzas, Your Pizzas
 fav_pizzas = ["pineapple", "mushroom", "pepperoni"]
