543dd8ef0f0bd81cfc279b218aa667057fdc05ea
smngo100 <smngo100@gmail.com>
2024-11-16 22:32:17 -0800

    Update Functions.py

--- Notes/Python/Python Crash Course/Chapter 08 - Functions/Functions.py
+++ Notes/Python/Python Crash Course/Chapter 08 - Functions/Functions.py
@@ -79,3 +79,18 @@ def build_person(first_name, last_name):
 
 musician = build_person('jimi', 'hendrix')
   print(musician)
+
+# Returning a Dictionary with Optional Values 
+# Optional value here is 'age'
+def build_person(first_name, last_name):
+    """Return a dictionary of information about a person."""
+  person = {'first': first_name, 'last': last_name}
+  if age: 
+    person['age'] = age
+  return person
+
+musician = build_person('jimi', 'hendrix', age = 27)
+  print(musician) 
+
+# Using a Function with a while Loop 
+
