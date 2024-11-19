d57f24111c75c59909411ed6ac8aa31559b03654
smngo100 <smngo100@gmail.com>
2024-11-18 23:07:46 -0800

    Update Functions.py

--- Notes/Python/Python Crash Course/Chapter 08 - Functions/Functions.py
+++ Notes/Python/Python Crash Course/Chapter 08 - Functions/Functions.py
@@ -113,3 +113,21 @@ def greet_users(names):
 
 usernames = ['hannah', 'ty', 'margot']
 greet_users(usernames) 
+
+# Modifying a List in a Function 
+def print_models(unprinted_designs, completed_models):
+    while unprinted_designs:
+        current_design = unprinted_designs.pop()
+        print(f"Printing mode: {current_design}")
+        completed_models.append(current_design)
+
+def show_completed_models(completed_models):
+    print(f"\n The following models that were printed.")
+    for completed_model in completed_models:
+        print(completed_model)
+
+unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+
+print_models(unprinted_designs, completed_models)
+show_completed_models(completed_models)
