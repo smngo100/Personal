292dbb40d94767b84446663420d87456cfe9062f
smngo100 <smngo100@gmail.com>
2024-11-18 22:46:51 -0800

    Update Functions.py

--- Notes/Python/Python Crash Course/Chapter 08 - Functions/Functions.py
+++ Notes/Python/Python Crash Course/Chapter 08 - Functions/Functions.py
@@ -93,4 +93,14 @@ musician = build_person('jimi', 'hendrix', age = 27)
   print(musician) 
 
 # Using a Function with a while Loop 
+def get_formatted_name(first_name, last_name): 
+  full_name = f"{first_name} {last_name}"
+  return full_name.title()
 
+while True: 
+  print(f"\nPlease tell me your name:")
+  f_name = input("First name: ")
+  l_name = input("Last name: ")
+
+  formatted_name = get_formatted_name(f_name, l_name) 
+  print(f"\nHello, {formatted_name}!")
