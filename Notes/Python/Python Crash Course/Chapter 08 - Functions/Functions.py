4fc94bd0c420bb41d455fbba83362fd5f94560e1
smngo100 <smngo100@gmail.com>
2024-11-18 22:48:51 -0800

    Update Functions.py

--- Notes/Python/Python Crash Course/Chapter 08 - Functions/Functions.py
+++ Notes/Python/Python Crash Course/Chapter 08 - Functions/Functions.py
@@ -104,3 +104,12 @@ while True:
 
   formatted_name = get_formatted_name(f_name, l_name) 
   print(f"\nHello, {formatted_name}!")
+
+# Passing a List 
+def greet_users(names): 
+  for name in names: 
+    msg = f"Hello, {name.title()}!"
+    print(msg) 
+
+usernames = ['hannah', 'ty', 'margot']
+greet_users(usernames) 
