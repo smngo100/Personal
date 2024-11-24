0a809f02317e08d9ef147620d7d1e0dad11df8ef
smngo100 <smngo100@gmail.com>
2024-11-23 23:26:26 -0800

    Update Importing a Module & Accessing the Functions.py

--- Notes/Python/Python Crash Course/Chapter 08 - Functions/Importing a Module & Accessing the Functions.py
+++ Notes/Python/Python Crash Course/Chapter 08 - Functions/Importing a Module & Accessing the Functions.py
@@ -7,6 +7,8 @@ Each function in the module is available through the following syntax:
 
 You can also import a SPECIFIC FUNCTION from a module: 
   from module_name import function_name
+* Note: when you import just the function you're going to use, with this syntax, you don't need to use the dot notation when yoou call a function. 
+        because we've explicitly imported the function in the import statement, we can call it by name when we use the function
 
 You can import as many functions as you want from a module by separating each function's name with a comma: 
   from module_name import function_0, function_1, function_2
