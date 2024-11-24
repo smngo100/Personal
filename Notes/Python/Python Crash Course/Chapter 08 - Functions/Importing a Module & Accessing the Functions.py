6f523135bacf0b06e73be63dd9aeab20a74cde03
smngo100 <smngo100@gmail.com>
2024-11-23 23:22:01 -0800

    Create Importing a Module & Accessing the Functions.py

---
+++ Notes/Python/Python Crash Course/Chapter 08 - Functions/Importing a Module & Accessing the Functions.py
@@ -0,0 +1,6 @@
+"""
+Module: a file ending in .py that contaisn the code you wan tto import into your program
+
+Each function in the module is available through the following syntax:
+  module_name.function_name() 
+"""
