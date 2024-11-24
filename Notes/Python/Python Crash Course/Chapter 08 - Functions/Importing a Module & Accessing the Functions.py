e622342e66d6e29a55a1cbecf89bdbb49cefef85
smngo100 <smngo100@gmail.com>
2024-11-23 23:23:08 -0800

    Update Importing a Module & Accessing the Functions.py

--- Notes/Python/Python Crash Course/Chapter 08 - Functions/Importing a Module & Accessing the Functions.py
+++ Notes/Python/Python Crash Course/Chapter 08 - Functions/Importing a Module & Accessing the Functions.py
@@ -3,4 +3,8 @@ Module: a file ending in .py that contaisn the code you wan tto import into your
 
 Each function in the module is available through the following syntax:
   module_name.function_name() 
+
+
+You can also import a SPECIFIC FUNCTION from a module: 
+  from module_name import function_name
 """
