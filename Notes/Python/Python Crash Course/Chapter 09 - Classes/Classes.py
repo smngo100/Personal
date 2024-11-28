3de5ba96876055bd2e7ace3e82a1eeb92bbcf738
smngo100 <smngo100@gmail.com>
2024-11-27 17:07:26 -0800

    Update Classes.py

--- Notes/Python/Python Crash Course/Chapter 09 - Classes/Classes.py
+++ Notes/Python/Python Crash Course/Chapter 09 - Classes/Classes.py
@@ -6,6 +6,16 @@ Object-oriented programming (OOP) is one of the most effective approaches to wri
 """
 
 Definitions
-# Instantiation: Making an object from a class, and you work with instances of a class.
+# Instantiation: Making an object from a class, and you work with instances of a class
+# Instance: An individual object created from a class
+# Method: A function that's part of a class
+# Attribute: Variables that are accessible through instances
+
+Notes 
+# By convention, capitalized names refer to classes in Python
+# No parentheses in the class definition
+
+The __init__() Method  # Special method that Python runs automatically whenever a new instanced based on the class
+# Any variable prefixed with self is available to every method in the class
 
 
