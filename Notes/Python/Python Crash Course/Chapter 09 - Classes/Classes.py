5703a46d5b666cf4677d755bf2b1eca8c1eab464
smngo100 <smngo100@gmail.com>
2024-11-27 16:36:51 -0800

    Create Classes.py

---
+++ Notes/Python/Python Crash Course/Chapter 09 - Classes/Classes.py
@@ -0,0 +1,11 @@
+"""
+Object-oriented programming (OOP) is one of the most effective approaches to writing software.
+* You write classes that represent real-world things and situations
+* You create objects based on these classes 
+* When you write a class, you define the general behavior that a whole category of objects can have
+"""
+
+Definitions
+# Instantiation: Making an object from a class, and you work with instances of a class.
+
+
