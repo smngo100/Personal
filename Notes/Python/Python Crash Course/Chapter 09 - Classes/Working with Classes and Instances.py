589f8ea279ee7d9d4e8ffc7c57cf1532a8046c52
smngo100 <smngo100@gmail.com>
2024-11-30 10:25:47 -0800

    Update Working with Classes and Instances.py

--- Notes/Python/Python Crash Course/Chapter 09 - Classes/Working with Classes and Instances.py
+++ Notes/Python/Python Crash Course/Chapter 09 - Classes/Working with Classes and Instances.py
@@ -1,8 +1,22 @@
-Setting a Default Value for an Attribute 
+# Setting a Default Value for an Attribute 
+class Car:
+
+def __init__(self, make, model, year):
+  """Initialize attributes to describe a car."""
+  self.make = make
+  self.model = model
+  self.year = year
+  self.odometer_reading = 0  # default value
 
 
+---------------------------------------- Modifying Attribute Values ----------------------------------------
+# Can change an attribute's value in 3 ways: 
+# 1) Change the value directly through an instance
+# 2) Set the value through a method 
+# 3) Increment the value (add a certain amount to it) through a method 
 
 
+# Modifying an Attribute's Value Directly 
 
 
 
