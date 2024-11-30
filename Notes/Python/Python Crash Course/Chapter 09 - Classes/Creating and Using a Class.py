3305f5372a554dcc4676e11d63a5c2834142518e
smngo100 <smngo100@gmail.com>
2024-11-30 10:17:22 -0800

    Update Creating and Using a Class.py

--- Notes/Python/Python Crash Course/Chapter 09 - Classes/Creating and Using a Class.py
+++ Notes/Python/Python Crash Course/Chapter 09 - Classes/Creating and Using a Class.py
@@ -5,30 +5,37 @@ Object-oriented programming (OOP) is one of the most effective approaches to wri
 * When you write a class, you define the general behavior that a whole category of objects can have
 """
 
+
 Definitions
 # Instantiation: Making an object from a class, and you work with instances of a class
 # Instance: An individual object created from a class
 # Method: A function that's part of a class
 # Attribute: Variables that are accessible through instances
 
+
 Notes 
 # By convention, capitalized names refer to classes in Python
 # No parentheses in the class definition
 
+
 The __init__() Method  # Special method that Python runs automatically whenever we create a new instanced based on the class
 # Any variable prefixed with self is available to every method in the class
 
+
 Making an Instance from a Class 
-# Create an instance 
+# Make an instance from the Dog class and assign it to the variable my_dog
 my_dog = Dog('Willie', 6)
+
 
 Accessing Attributes 
 # To access the attributes of an instance, you use DOT NOTATION. 
-  my_dog.name 
+variable_name.attribute_name
+my_dog.name 
+
 
 Calling Methods 
 # Use dot notation to call any method 
-  my_dog.sit() 
-  my_dog.roll_over()
+my_dog.sit() 
+my_dog.roll_over()
 
 
