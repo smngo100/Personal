6a10d4fa3aa951ffa925fa4d945cdbcd8687bf9a
smngo100 <smngo100@gmail.com>
2024-11-30 10:44:48 -0800

    Update Working with Classes and Instances.py

--- Notes/Python/Python Crash Course/Chapter 09 - Classes/Working with Classes and Instances.py
+++ Notes/Python/Python Crash Course/Chapter 09 - Classes/Working with Classes and Instances.py
@@ -26,4 +26,33 @@ my_new_car.read_odometer()
 
 
 ##### Modifying an Attribute's Value Through a Method #####
+class Car:
+    --snip--
 
+    def update_odometer(self, mileage):
+        """Set the odometer reading to the given value."""
+        self.odometer_reading = mileage
+      
+my_new_car.update_odometer(23)
+my_new_car.read_odometer()
+
+
+##### Incrementing an Attribute's Value Through a Method #####
+class Car:
+    --snip--
+
+    def update_odometer(self, mileage):
+        --snip--
+
+    def increment_odometer(self, miles):
+        """Add the given amount to the odometer reading."""
+        self.odometer_reading += miles
+
+❶ my_used_car = Car('subaru', 'outback', 2019)
+print(my_used_car.get_descriptive_name())
+
+❷ my_used_car.update_odometer(23_500)
+my_used_car.read_odometer()
+
+my_used_car.increment_odometer(100)
+my_used_car.read_odometer()
