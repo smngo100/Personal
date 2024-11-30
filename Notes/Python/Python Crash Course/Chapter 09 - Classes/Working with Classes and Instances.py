481749e0414af9ad0d81b6f2a1fbe330415b5f24
smngo100 <smngo100@gmail.com>
2024-11-30 10:46:55 -0800

    Update Working with Classes and Instances.py

--- Notes/Python/Python Crash Course/Chapter 09 - Classes/Working with Classes and Instances.py
+++ Notes/Python/Python Crash Course/Chapter 09 - Classes/Working with Classes and Instances.py
@@ -48,10 +48,10 @@ class Car:
         """Add the given amount to the odometer reading."""
         self.odometer_reading += miles
 
-❶ my_used_car = Car('subaru', 'outback', 2019)
+my_used_car = Car('subaru', 'outback', 2019)
 print(my_used_car.get_descriptive_name())
 
-❷ my_used_car.update_odometer(23_500)
+my_used_car.update_odometer(23_500)
 my_used_car.read_odometer()
 
 my_used_car.increment_odometer(100)
