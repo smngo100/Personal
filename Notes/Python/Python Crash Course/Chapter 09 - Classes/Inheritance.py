0d45722fa858dbc153d49d795796aa6730df7ddc
smngo100 <smngo100@gmail.com>
2024-12-01 22:16:32 -0800

    Create Inheritance.py

---
+++ Notes/Python/Python Crash Course/Chapter 09 - Classes/Inheritance.py
@@ -0,0 +1,10 @@
+# Inheritance: When one class inherits from another, it takes on the attributes and methods of the first class.
+# Parent class: The original class 
+# Child class: The new class that can inherit any or all of the attributes and methods of its parent class, but it's also free to define new attributes and methods of its own.
+
+
+
+
+
+
+
