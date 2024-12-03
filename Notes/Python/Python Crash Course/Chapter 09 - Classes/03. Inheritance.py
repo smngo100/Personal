3b9713628150eddc4234ebe2e858e49b6f7410ec
smngo100 <smngo100@gmail.com>
2024-12-02 22:34:52 -0800

    Update 03. Inheritance.py

--- Notes/Python/Python Crash Course/Chapter 09 - Classes/03. Inheritance.py
+++ Notes/Python/Python Crash Course/Chapter 09 - Classes/03. Inheritance.py
@@ -19,7 +19,7 @@ class Car:
 
 class ElectricCar(Car): 
   def __init__(self, make, model, year): 
-    super.__init__(make, model, year) 
+    super().__init__(make, model, year) 
     self.battery_size = 40  # attribute specific to the child class
 
 
