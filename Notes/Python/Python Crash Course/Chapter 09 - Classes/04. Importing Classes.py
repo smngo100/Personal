63ddbe65735907887e86c135aa8ee620fe3c758f
smngo100 <smngo100@gmail.com>
2024-12-04 23:03:09 -0800

    Update 04. Importing Classes.py

--- Notes/Python/Python Crash Course/Chapter 09 - Classes/04. Importing Classes.py
+++ Notes/Python/Python Crash Course/Chapter 09 - Classes/04. Importing Classes.py
@@ -6,9 +6,48 @@
 class Car: 
   --snip--
 
+# Import the Car class and create an instance from that class 
+from car import Car 
+  my_new_car = Car('audi', 'a4', 2024)
+  --snip--
+# The import statement tells Python to open the car module and import the class Car. Now we can use the Car class as if it were defined in this file. 
 
 
+##### Storing Multiple Classes IN a Module #####
+# You can store as many classes as you need in a single module, although each class in a module should be related somehow.
+class Car: 
+  --snip--
 
+class Battery: 
+  --snip--
+
+class ElectricCar(Car): 
+  --snip--
+
+my_electric_car.py 
+from car import ElectricCar 
+
+
+##### Importing Multiple Classes FROM a Module #####
+my_cars.py
+from car import Car, ElectricCar
+# You import multiple classes from a module by separating each class with a comma 
+
+
+##### Importing an Entire Module ##### 
+import car 
+  my_new_car = car.Car('audi', 'a4', 2024)  
+  --snip-- 
+# Syntax: 
+module_name.ClassName
+
+
+##### Importing All Classes from a Module #####
+# You can import every class from a module using the following syntax: 
+from module_name import * 
+
+
+##### Importing a Module into a Module #####
 
 
 
