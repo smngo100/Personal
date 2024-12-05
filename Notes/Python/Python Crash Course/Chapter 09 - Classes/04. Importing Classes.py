a43e6e88bcaf5e7d64128d177101800e48c48c69
smngo100 <smngo100@gmail.com>
2024-12-04 22:45:20 -0800

    Update 04. Importing Classes.py

--- Notes/Python/Python Crash Course/Chapter 09 - Classes/04. Importing Classes.py
+++ Notes/Python/Python Crash Course/Chapter 09 - Classes/04. Importing Classes.py
@@ -1,4 +1,11 @@
 ##### Importing a Single Class #####
+# Module-level docstring briefly describes the contents of the module. You shuold write a docstring for each module you create
+# Example: (It's written above the class)
+"""A class that can be used to represent a car."""
+
+class Car: 
+  --snip--
+
 
 
 
