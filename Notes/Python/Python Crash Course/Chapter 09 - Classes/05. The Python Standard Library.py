9a98843b8715fd255006d116de98eb91a95b0f7d
smngo100 <smngo100@gmail.com>
2024-12-08 23:08:17 -0800

    Create 05. The Python Standard Library.py

---
+++ Notes/Python/Python Crash Course/Chapter 09 - Classes/05. The Python Standard Library.py
@@ -0,0 +1,9 @@
+# The Python standard library is a set of modules included with every Python installation 
+# Functions from the random module: randint(), choice(), etc 
+
+# Syntax 
+import random 
+
+# or 
+
+from random import function_name
