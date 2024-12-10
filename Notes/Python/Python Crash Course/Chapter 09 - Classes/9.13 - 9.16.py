1f603383c4761879a9772cef76106dd94a09937d
smngo100 <smngo100@gmail.com>
2024-12-10 00:02:25 -0800

    Update 9.13 - 9.16.py

--- Notes/Python/Python Crash Course/Chapter 09 - Classes/9.13 - 9.16.py
+++ Notes/Python/Python Crash Course/Chapter 09 - Classes/9.13 - 9.16.py
@@ -32,18 +32,44 @@ print()
 # 9-14. Lottery
 # List containing a series of 10 numbers and 5 letters
 series_of_num_letters = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e']
-random_selection = random.choice(series_of_num_letters)
-print("Any ticket matching these 4 numbers or letters wn a prize: ")
+print("Any ticket matching these 4 numbers or letters win a prize: ")
+
+winning_ticket = []
 for i in range(4):
-    print(random_selection)
+    # Randomly select 4 numbers or letters from the list
+    ticket = random.choice(series_of_num_letters)
+    winning_ticket.append(ticket)
+    print(f"Winning tickets: {winning_ticket}")
 
 
 # 9-15. Lottery Analysis
+my_ticket = []
+print("Pulling numbers until your ticket wins.")
+while True:
+    ticket = random.choice(series_of_num_letters)
+    print(f"\tPulled ticket: {ticket}")
 
+    if ticket in winning_ticket:
+        print(f"{ticket} is a winning ticket!")
+        break
+    else:
+        print("\tPull again.\n")
 
 
 # 9-16. Python Module of the Week
 
+series_of_num_letters = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e']
+winning_tickets = []
 
+while True:
+    lottery_winners = random.choice(series_of_num_letters)
 
+    print(f"Winning tickets: {lottery_winners}")
+    winning_tickets.append(lottery_winners)
 
+    print(f"Pulling numbers until your ticket wins.")
+    if ticket in winning_ticket:
+        print(f"{ticket} is a winning ticket!")
+        break
+    else:
+        print("\tPull again.\n")
