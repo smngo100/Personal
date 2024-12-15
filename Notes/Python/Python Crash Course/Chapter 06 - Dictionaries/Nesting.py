702146abd7e02d484fd19ca11e78b13e435638ec
smngo100 <smngo100@gmail.com>
2024-12-14 23:47:36 -0800

    Update Nesting.py

--- Notes/Python/Python Crash Course/Chapter 06 - Dictionaries/Nesting.py
+++ Notes/Python/Python Crash Course/Chapter 06 - Dictionaries/Nesting.py
@@ -1,4 +1,5 @@
 # A List of Dictionaries
+# Alien example
 alien_0 = {'color': 'green', 'points': 5}
 alien_1 = {'color': 'yellow', 'points': 10}
 alien_2 = {'color': 'red', 'points': 15}
@@ -8,16 +9,18 @@ aliens = [alien_0, alien_1, alien_2]
 for alien in aliens:
     print(alien)
 
-# A List in a Dictionary 
+
+# A List in a Dictionary
 # Pizza example
 pizza = {
     'crust': 'thick',
     'toppings': ['mushrooms', 'extra cheese'],
-    }
+}
 print(f"You ordered a {pizza['crust']}-crust pizza with the following toppings:")
 
 for topping in pizza['toppings']:
-  print(f"\t{topping}")
+    print(f"\t{topping}")
+
 
 # Languages example
 favorite_languages = {
@@ -25,34 +28,34 @@ favorite_languages = {
     'sarah': ['c'],
     'edward': ['rust', 'go'],
     'phil': ['python', 'haskell'],
-    }
+}
 
 for name, languages in favorite_languages.items():
     print(f"\n{name.title()}'s favorite languages are:")
     for language in languages:
         print(f"\t{language.title()}")
 
-# A Dictionary in a Dictionary 
+
+# A Dictionary in a Dictionary
 users = {
     'aeinstein': {
         'first': 'albert',
         'last': 'einstein',
         'location': 'princeton',
-        },
+    },
 
     'mcurie': {
         'first': 'marie',
         'last': 'curie',
         'location': 'paris',
-        },
+    },
 
-    }
+}
 
 for username, user_info in users.items():
     print(f"\nUsername: {username}")
     full_name = f"{user_info['first']} {user_info['last']}"
     location = user_info['location']
-    
+
     print(f"\tFull name: {full_name.title()}")
     print(f"\tLocation: {location.title()}")
-
