9a8bebed4b30d23104254653f9253dda02f0e44f
smngo100 <smngo100@gmail.com>
2024-12-14 23:31:30 -0800

    Update  4.3 - 4.9.py

--- Notes/Python/Python Crash Course/Chapter 04 - Working with Lists/ 4.3 - 4.9.py
+++ Notes/Python/Python Crash Course/Chapter 04 - Working with Lists/ 4.3 - 4.9.py
@@ -3,12 +3,14 @@ for i in range(1, 21):
     print(i)
 print()
 
+
 # 4-4. One Million
 num = []
 for i in range(1, 1000001):
     print(i)
     num.append(i)
 print()
+
 
 # 4-5. Summing a Million
 num = []
@@ -20,10 +22,12 @@ print(f"Max num: {max(num)}")
 print(f"Sum num: {sum(num)}")
 print()
 
+
 # 4-6. Odd Numbers
 for i in range(1, 20, 2):    # for i in range(first value, second value, step size)    aka    # for i in range(start, stop, step size)
     print(i)
 print()
+
 
 # 4-7. Threes
 multiples_of_3 = []
@@ -38,12 +42,14 @@ for i in multiples_of_three:
     print(i)
 print()"""
 
+
 # 4-8. Cubes
 cubes = []
 for i in range(1, 11):
     cube = i ** 3
     cubes.append(cube)
 print(cubes)
+
 
 # 4-9. Cube Comprehension
 cubes = [cube ** 3 for cube in range(1, 11)]
