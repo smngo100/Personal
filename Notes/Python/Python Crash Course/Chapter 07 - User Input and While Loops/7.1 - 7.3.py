2de65431f4d206304e591a24c5ea577a6d9b0291
smngo100 <smngo100@gmail.com>
2024-12-18 22:34:11 -0800

    Update 7.1 - 7.3.py

--- Notes/Python/Python Crash Course/Chapter 07 - User Input and While Loops/7.1 - 7.3.py
+++ Notes/Python/Python Crash Course/Chapter 07 - User Input and While Loops/7.1 - 7.3.py
@@ -2,16 +2,18 @@
 rental_car = input("What kind of rental car would you like? ")
 print(f"Let me see if I can find you a {rental_car.title()}.")
 
+
 # 7-2. Restaurant Seating
-people = int(input("How many people are in your dinner group? "))
+people = int(input("\nHow many people are in your dinner group? "))
 
 if people > 8:
     print("You'll have to wait for a table.")
 else:
     print("Your table is ready.")
 
+
 # 7-3. Multiples of Ten
-number = int(input("Enter a number: "))
+number = int(input("\nEnter a number: "))
 
 if number % 10 == 0:
     print(f"{number} is a multiple of 10.")
