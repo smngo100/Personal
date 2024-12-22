d878038da772447872be9d797c6b26e80db859a7
smngo100 <smngo100@gmail.com>
2024-12-22 01:06:10 -0800

    Update 8.15 - 8.17.py

--- Notes/Python/Python Crash Course/Chapter 08 - Functions/8.15 - 8.17.py
+++ Notes/Python/Python Crash Course/Chapter 08 - Functions/8.15 - 8.17.py
@@ -6,20 +6,26 @@ printing_functions.print_models(unprinted_designs='phone case', 'robot pendant',
 
 
 # 8-16. Imports.
+# Importing an entire module
 import pizza
 pizza.make_pizza(16, 'pepperoni')
 
+# Importing a specific function from a module
 from pizza import make_pizza
 make_pizza(16, 'mushrooms')
 
+# Importing a function from a module as an alias
 from pizza import make_pizza as mp
 mp(16, 'green peppers')
 
+# Importing a module as an alias 
 import pizza as pza
 pza.make_pizza(16, 'pineapple')
 
+# Importing all functions in a module 
 from pizza import *
 make_pizza(16, 'vegetables')
+
 
 # 8-17. Styling Functions.
 # The three programs follow the styling guidelines described in this section
