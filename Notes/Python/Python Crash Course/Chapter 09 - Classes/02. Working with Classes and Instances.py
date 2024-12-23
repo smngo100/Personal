b4f5528b7c7d55c50dd90b096231e5c9e9da57f6
smngo100 <smngo100@gmail.com>
2024-12-22 22:11:55 -0800

    Update 02. Working with Classes and Instances.py

--- Notes/Python/Python Crash Course/Chapter 09 - Classes/02. Working with Classes and Instances.py
+++ Notes/Python/Python Crash Course/Chapter 09 - Classes/02. Working with Classes and Instances.py
@@ -1,26 +1,25 @@
 ##### Setting a Default Value for an Attribute #####
 class Car:
+    def __init__(self, make, model, year):
+      """Initialize attributes to describe a car."""
+      self.make = make
+      self.model = model
+      self.year = year
+      self.odometer_reading = 0  # default value
 
-def __init__(self, make, model, year):
-  """Initialize attributes to describe a car."""
-  self.make = make
-  self.model = model
-  self.year = year
-  self.odometer_reading = 0  # default value
 
-
----------------------------------------- Modifying Attribute Values ----------------------------------------
-# Can change an attribute's value in 3 ways: 
-# 1) Change the value directly through an instance
-# 2) Set the value through a method 
-# 3) Increment the value (add a certain amount to it) through a method 
+#---------------------------------------- Modifying Attribute Values ----------------------------------------
+# Can change an attribute's value in 3 ways:
+#   1) Change the value directly through an instance
+#   2) Set the value through a method
+#   3) Increment the value (add a certain amount to it) through a method
 
 
 ##### Modifying an Attribute's Value Directly #####
-# Use DOT NOTATION to access the car's odometer_reading attribute, and set its value directly 
+# Use DOT NOTATION to access the car's odometer_reading attribute, and set its value directly
 class Car:
     --snip--
-  
+
 my_new_car.odometer_reading = 23
 my_new_car.read_odometer()
 
@@ -30,9 +29,9 @@ class Car:
     --snip--
 
     def update_odometer(self, mileage):
-        #"""Set the odometer reading to the given value."""
+        """Set the odometer reading to the given value."""
         self.odometer_reading = mileage
-      
+
 my_new_car.update_odometer(23)
 my_new_car.read_odometer()
 
@@ -43,9 +42,9 @@ class Car:
 
   def update_odometer(self, mileage):
     --snip--
-  
+
   def increment_odometer(self, miles):
-    #"""Add the given amount to the odometer reading."""
+    """Add the given amount to the odometer reading."""
     self.odometer_reading += miles
 
 my_used_car = Car('subaru', 'outback', 2019)
