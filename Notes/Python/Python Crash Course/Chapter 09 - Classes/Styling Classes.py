80554c2315a98d816a26a2f670d03671968af640
smngo100 <smngo100@gmail.com>
2024-12-27 12:39:56 -0800

    Create Styling Classes.py

---
+++ Notes/Python/Python Crash Course/Chapter 09 - Classes/Styling Classes.py
@@ -0,0 +1,11 @@
+"""
+Class names should be written in CamelCase
+
+Every class should have a docstring immediately following the class definition 
+* Docstring should be brief description of what the class does
+* Module should have a docstring describing what the classes in a module can be used for 
+
+Use balnk lines to organize code 
+* Within a class can use one blank line between methods 
+* Within a module can use two blank lines to separate classes 
+"""
