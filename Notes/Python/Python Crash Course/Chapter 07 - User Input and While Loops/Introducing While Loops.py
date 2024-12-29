dff2e3cba0c98dba1022457546f23dbd7d05b881
smngo100 <smngo100@gmail.com>
2024-12-29 12:10:12 -0800

    Update Introducing While Loops.py

--- Notes/Python/Python Crash Course/Chapter 07 - User Input and While Loops/Introducing While Loops.py
+++ Notes/Python/Python Crash Course/Chapter 07 - User Input and While Loops/Introducing While Loops.py
@@ -5,7 +5,7 @@
 """
 For a program that should run only as long as many conditions are true, you can define one variable that determines whether or not the entire program is active. 
 This variable, called a flag, acts as a signal to the program. 
-We can write our programs so they run while the flag is set to True and stop running when any of several events sets the value fo the flag to False.
+We can write our programs so they run while the flag is set to True and stop running when any of several events sets the value of the flag to False.
 """
 
 prompt = "\nTell me something, and I will repeat it back to you:"
