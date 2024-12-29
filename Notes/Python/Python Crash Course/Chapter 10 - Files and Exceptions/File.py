15d2b07864c912745633864e143ea097ff885820
smngo100 <smngo100@gmail.com>
2024-12-28 22:08:40 -0800

    Update File.py

--- Notes/Python/Python Crash Course/Chapter 10 - Files and Exceptions/File.py
+++ Notes/Python/Python Crash Course/Chapter 10 - Files and Exceptions/File.py
@@ -33,6 +33,18 @@ for line in lines:
 
 
 ##### Working with a File's Contents ##### 
+from pathlib import Path
+
+path = Path('pi_digits.txt')
+contents = path.read_text()
+
+lines = contents.splitlines()
+pi_string = ''
+for line in lines:
+    pi_string += line.lstrip()
+
+print(pi_string)
+print(len(pi_string))
 
 
 
