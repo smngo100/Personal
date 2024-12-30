b65781a16f2f3a4ca5481edbebeadb43f6dfb10b
smngo100 <smngo100@gmail.com>
2024-12-29 20:37:27 -0800

    Update Exceptions.py

--- Notes/Python/Python Crash Course/Chapter 10 - Files and Exceptions/Exceptions.py
+++ Notes/Python/Python Crash Course/Chapter 10 - Files and Exceptions/Exceptions.py
@@ -36,4 +36,21 @@ while True:
 
 
 ##### Handling the FileNotFoundError Exception ##### 
+"""
+It's often best to start at the very end of the traceback. On the last last, we see the exception that was raised.
+This is important because it tells us what kind of exception to us in the except block that we'll write.
+The beginning of the tracebook shows where the error occured. The next line shows the line of code that caused the error. 
+To handle the error that's being raised, the try block will begin with the line that was identified as problematic in the traceback.
+* The try block code is basically the line of code that caused the error. 
+"""
+
+# Example 
+from path import Path 
+path = Path('alice.txt') 
+contents = path.read_text(encoding='utf-8) 
+# We'd get a traceback error of the line contents . . . because alice.txt isn't in our path. 
+# We can handle the error weith a try block 
+try: 
+    contents = path.read_text(encoding='utf-8) 
+                        
 
