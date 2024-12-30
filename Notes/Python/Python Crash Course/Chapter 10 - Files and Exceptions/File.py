48ceb4926b25653b3d831d8b22a592d97ba9519e
smngo100 <smngo100@gmail.com>
2024-12-29 21:38:05 -0800

    Update File.py

--- Notes/Python/Python Crash Course/Chapter 10 - Files and Exceptions/File.py
+++ Notes/Python/Python Crash Course/Chapter 10 - Files and Exceptions/File.py
@@ -61,12 +61,7 @@ from pathlib import Path
 
 contents = "I love programming.\n"
 contents += "I love creating new games.\n"
-cotnents += "I also love working with data.n\"
+contents += "I also love working with data.n\"
 
 path = Path('programming.txt') 
 path.write_text(contents) 
-
-
-##### Analyzing Text ##### 
-# string method split() default splits a string wherever it finds any whitespace 
-
