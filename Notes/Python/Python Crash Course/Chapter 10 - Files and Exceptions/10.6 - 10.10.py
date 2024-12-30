b5238ea0e169e6c0b9e931fd4f5f3f52c6c7e5cd
smngo100 <smngo100@gmail.com>
2024-12-29 22:41:00 -0800

    Create 10.6 - 10.10.py

---
+++ Notes/Python/Python Crash Course/Chapter 10 - Files and Exceptions/10.6 - 10.10.py
@@ -0,0 +1,52 @@
+# 10-6. Addition
+"""Prompts user for two numbers and prints their sum."""
+# Using while loop
+while True:
+
+    num1 = input("\nEnter the first number: ")
+    if num1 == 'q':
+        break
+    try:
+        num1 = int(num1)
+    except ValueError:
+        print("You must enter a number.")
+
+
+    num2 = input("Enter the second number: ")
+    if num2 == 'q':
+        break
+    try:
+        num2 = int(num2)
+    except ValueError:
+        print("You must enter a number.")
+    else:
+        result = num1 + num2
+        print(result)
+
+
+# Not using while loop
+try:
+    num1 = int(input("\nEnter the first number: "))
+    num2 = int(input("Enter the second number: "))
+    result = num1 + num2
+except ValueError:
+    print("You must enter a number.")
+else:
+    print(sum)
+
+
+# 10-7. Addition Calculator
+
+
+
+
+# 10-8. Cats and Dogs
+
+
+
+# 10-9. Silent Cats and Dogs
+
+
+
+
+# 10-10. Common Words
