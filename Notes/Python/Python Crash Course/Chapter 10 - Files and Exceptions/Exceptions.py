42237d5f0f7592565c40b0da501ef8ddd80ef981
smngo100 <smngo100@gmail.com>
2024-12-29 21:51:19 -0800

    Update Exceptions.py

--- Notes/Python/Python Crash Course/Chapter 10 - Files and Exceptions/Exceptions.py
+++ Notes/Python/Python Crash Course/Chapter 10 - Files and Exceptions/Exceptions.py
@@ -54,3 +54,30 @@ try:
     contents = path.read_text(encoding='utf-8) 
 except FileNotFoundError:                         
     print(f"Sorry, the file {path} does not exist.")
+
+
+##### Analyzing Text ##### 
+# string method split() default splits a string wherever it finds any whitespace 
+from pathlib import Path
+path = Path('alice.txt')
+try:
+    contents = path.read_text(encoding='utf-8')
+except FileNotFoundError:
+    print(f"Sorry, the file {path} does not exist.")
+else:
+    words = contents.split()
+    num_words = len(words)
+    print(f"The file {path} has about {num_words} words.")
+
+##### Working with Multiple Files ##### 
+from pathlib import Path
+def count_words(filename):
+    --snip--
+filenames = ['alice.txt', 'siddhartha.txt', 'moby_dick.txt', 
+        'little_women.txt']
+for filename in filenames:
+    path = Path(filename)
+    count_words(path)
+
+
+
