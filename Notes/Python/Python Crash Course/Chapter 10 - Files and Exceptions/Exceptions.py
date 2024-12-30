edcfce78f871f3444947e315b4d98fc72ca52798
smngo100 <smngo100@gmail.com>
2024-12-29 18:43:27 -0800

    Update Exceptions.py

--- Notes/Python/Python Crash Course/Chapter 10 - Files and Exceptions/Exceptions.py
+++ Notes/Python/Python Crash Course/Chapter 10 - Files and Exceptions/Exceptions.py
@@ -4,13 +4,17 @@
 # Try-except block asks Python to do something, but it also tells Python what to do if an exception is raised.
 
 ##### Using try-except Blocks #####
+# When you think an error may occur, you can write a try-except block to handle the exception that might be raised.
 try:
     print(5/0)
 except ZeroDivisionError:
     print("You can't divide by zero!")
 print()
 
-##### Using Exceptions to Prevent Crashes #####
+
+##### The else Block #####
+# Any code that depends on the try block executing successfully goes in the else block
+# If the division operation is successful, we use the else block to print the result
 print("Give me two numbers, and I'll divide them.")
 print("Enter 'q' to quit.")
 
@@ -28,4 +32,8 @@ while True:
         print("You can't divide by 0!")
     else:
         print(answer)
+# The only code that should go in a try block is code that might cause an expcetion to be raised.
+
+
+##### Handling the FileNotFoundError Exception ##### 
 
