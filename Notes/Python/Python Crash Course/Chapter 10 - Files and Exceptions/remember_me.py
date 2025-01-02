c26a0eda511b07f5e8894140cf59734f91e3a011
smngo100 <smngo100@gmail.com>
2025-01-01 23:39:59 -0800

    Update remember_me.py

--- Notes/Python/Python Crash Course/Chapter 10 - Files and Exceptions/remember_me.py
+++ Notes/Python/Python Crash Course/Chapter 10 - Files and Exceptions/remember_me.py
@@ -10,6 +10,13 @@ def get_stored_username(path):
     else:
         return None
 
+def get_new_username(path):
+    """Prompt for a new username."""
+    username = input("What is your name? ")
+    contents = json.dumps(username)
+    path.write_text(contents)
+    return username
+
 def greet_user():
     """Greet the user by name."""
     path = Path('username.json')
@@ -17,9 +24,7 @@ def greet_user():
     if username:
      print(f"Welcome back, {username}!")
     else:
-        username = input("What is your name? ")
-        contents = json.dumps(username)
-        path.write_text(contents)
+        username = get_new_username(path)
         print(f"We'll remember you when you come back, {username}!")
 
 greet_user()
