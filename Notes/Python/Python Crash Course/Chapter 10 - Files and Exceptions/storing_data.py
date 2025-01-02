a43980a3d5813286f54e965d992f6502e393b496
smngo100 <smngo100@gmail.com>
2025-01-01 22:34:05 -0800

    Create storing_data.py

---
+++ Notes/Python/Python Crash Course/Chapter 10 - Files and Exceptions/storing_data.py
@@ -0,0 +1,8 @@
+"""JSON (JavaScript Object Notation) allows you to convert simple Python data structures into JSON-formatted strings,
+and then load the data from that file the next time the program runs. """
+
+##### json.dumps() and json.loads()
+
+
+
+
