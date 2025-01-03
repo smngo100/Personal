c9d049680e9b4e7a4c36f28d05311000eb54d95b
smngo100 <smngo100@gmail.com>
2025-01-03 09:44:14 -0800

    Update notes.py

--- Notes/C++/notes.py
+++ Notes/C++/notes.py
@@ -1,2 +1,4 @@
 # Statement: A type of instruction that causes the program to perform some action. 
 # Smallest independent unit of computation in C++
+
+# Function: A collection of statements that get executed sequentially (in order, from top to bottom) 
