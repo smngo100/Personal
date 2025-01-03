c840f61894ae33ef86a08e33b87272ea6f2ab09e
smngo100 <smngo100@gmail.com>
2025-01-03 15:42:21 -0800

    Create comments.cpp

---
+++ Notes/C++/comments.cpp
@@ -0,0 +1,21 @@
+///// Comments ///// 
+// Single-line comments
+// The // symbole begins a C++ single-line comment, which tells the compiler to ignore evertyhing from the // symbol to the end of the line.
+
+// Multi-line comments 
+// The /* and */ pair of symbols denote a C-style multi-line comment 
+
+/*
+Proper use of comments
+They should be used for three things: 
+1) For a given library, program, or function that are best used to describe WHAT the library, program, or function, does.
+Typically placed at the top of the file or immediately preceding the function.
+2) Within a library, program or function, comments can be used to describe HOW the code is going to accomplish its goal
+3) At the statement level, comments should be used to describe WHY the code is doing something. A bad statement comment explains what the code is doing.
+
+To summarize, write down the...
+1) What 
+2) How
+3) Why 
+... of your code in human language.
+*/
