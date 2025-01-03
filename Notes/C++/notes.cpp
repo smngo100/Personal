e3f685a3fe63c2a14f05c00fddf57db1d36c9cea
smngo100 <smngo100@gmail.com>
2025-01-03 15:26:59 -0800

    Update and rename notes.py to notes.cpp

---
+++ Notes/C++/notes.cpp
@@ -0,0 +1,18 @@
+///// Statement /////
+// Statement: A type of instruction that causes the program to perform some action. 
+// Smallest independent unit of computation in C++
+
+///// Functions /////
+// Function: A collection of statements that get executed sequentially (in order, from top to bottom) 
+// Functions are typically written to do a specific job or perform some useful action.
+
+// Identifier: The name of a function (or object, type, template, etc...)
+
+///// Comments ///// 
+// Single-line comments
+// The // symbole begins a C++ single-line comment, which tells the compiler to ignore evertyhing from the // symbol to the end of the line.
+
+// Multi-line comments 
+// The /* and */ pair of symbols denote a C-style multi-line comment 
+
+
