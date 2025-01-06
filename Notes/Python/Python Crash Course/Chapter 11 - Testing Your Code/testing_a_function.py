19d6b4923b6fdd07353242d477cd0e7fa4a67a18
smngo100 <smngo100@gmail.com>
2025-01-05 20:39:34 -0800

    Update testing_a_function.py

--- Notes/Python/Python Crash Course/Chapter 11 - Testing Your Code/testing_a_function.py
+++ Notes/Python/Python Crash Course/Chapter 11 - Testing Your Code/testing_a_function.py
@@ -1,1 +1,19 @@
+##### Unit Tests and Test Cases ##### 
+# A 'UNIT TEST' verifies that one specific aspect of a function's behavior is correct. 
+# A 'TEST CAST' is a collection of unit tests that together prove that a function behaves as it's supposed to, within the full range of sutations you expect it to handle.
+# A test case with 'FULL COVERAGE' includes a full range of unit tests covering all the possible ways you can use a function.
+
+
+##### A Passing Test ##### 
+# The name of a test file MUST start with test_
+# because when we ask pytest to run the tests we've written, it will look for any file that begins with test_, and run all of the tests it finds in that file. 
+# Test names should be longer and more descriptive than a typical function name.
+# An assertion is a claim about a condition.
+
+
+##### Runnning a Test ##### 
+# To let pytest run the test file...
+# Open a termal window and navigate to the folder that contains the test file.
+# In the terminal window, enter the command pytest
+
 
