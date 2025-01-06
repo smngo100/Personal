586fdbd76c2d8c7b0bbadba6440d7938812480c8
smngo100 <smngo100@gmail.com>
2025-01-05 18:00:10 -0800

    Create installing_pytest_w_pip.py

---
+++ Notes/Python/Python Crash Course/Chapter 11 - Testing Your Code/installing_pytest_w_pip.py
@@ -0,0 +1,11 @@
+# A third-party package is a library that's developed outside the core Python language.
+
+##### Updating pip #####
+# Pip: A tool used to install third-party packages 
+# Command to update any third-party package installed on your system: 
+$ python -m pip install --upgrade package_name  # might not need $
+
+
+##### Installing pytest #####
+# Command to install many third-party packages: 
+$ python -m pip install --user package_name  # might not need $  
