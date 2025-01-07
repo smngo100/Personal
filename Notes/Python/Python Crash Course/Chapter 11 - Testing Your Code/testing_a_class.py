8a7527c22717b707003a62ecb1aaf245b320dc94
smngo100 <smngo100@gmail.com>
2025-01-06 22:32:02 -0800

    Create testing_a_class.py

---
+++ Notes/Python/Python Crash Course/Chapter 11 - Testing Your Code/testing_a_class.py
@@ -0,0 +1,14 @@
+##### A Variety of Assertions ##### 
+# Commonly Used Assertion Statements in Tests 
+Assertion                       Claim 
+assert a == b                   Assert that two values are equal.
+assert a != b                   Assert that two values are not equal.
+assert a                        Assert that a evaluates to True. 
+assert not a                    Assert that a evaluates to False.
+assert element in list          Assert that an element is in a list.
+assert element not in list      Assert that an element is not in a list. 
+# **NOTE**: These are just a few examples; anything that can be expressed as a conditional statement can be included in a test.
+
+
+##### A Class to Test #####
+
