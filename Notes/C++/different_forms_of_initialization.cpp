e7e2fbd2a32748e3bc9a0d4d05ec62d437be8479
smngo100 <smngo100@gmail.com>
2025-01-08 17:10:18 -0800

    Create different_forms_of_initialization.cpp

---
+++ Notes/C++/different_forms_of_initialization.cpp
@@ -0,0 +1,11 @@
+// 5 common forms of initialization: 
+
+int a;        // default-initialization (no initializer)
+
+// Traditional initialization forms:
+int b = 5;    // copy-initialization (initial value after equals sign)
+int c (6);    // direct-initialization (initial value in parenthesis)
+
+// Modern initialization forms (preferred):
+int d { 7 };  // copy-initialization (initial value in bracesn)
+int e {};     // value-initialization (empty braces)
