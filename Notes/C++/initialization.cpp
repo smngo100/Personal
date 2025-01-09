81f540525325606717e7ba022f5128f257e85acd
smngo100 <smngo100@gmail.com>
2025-01-08 23:03:32 -0800

    Update initialization.cpp

--- Notes/C++/initialization.cpp
+++ Notes/C++/initialization.cpp
@@ -55,3 +55,19 @@ int a = 5, b = 5; // correct: a and b are initialized to 5
 To address such cases, C++17 introduced the [[maybe_unused]] attribute, which allows us 
 to tell the compiler that we’re okay with a variable being unused. The compiler will not 
 generate unused variable warnings for such variables. */
+
+#include <iostream>
+
+int main()
+{
+    [[maybe_unused]] double pi { 3.14159 };  // Don't complain if pi is unused
+    [[maybe_unused]] double gravity { 9.8 }; // Don't complain if gravity is unused
+    [[maybe_unused]] double phi { 1.61803 }; // Don't complain if phi is unused
+
+    std::cout << pi << '\n';
+    std::cout << phi << '\n';
+
+    // The compiler will no longer warn about gravity not being used
+
+    return 0;
+}
