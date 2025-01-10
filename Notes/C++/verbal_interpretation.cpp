de15fd827c57ffac852f120d119301562467efcf
smngo100 <smngo100@gmail.com>
2025-01-09 21:07:29 -0800

    Create verbal_interpretation.cpp

---
+++ Notes/C++/verbal_interpretation.cpp
@@ -0,0 +1,16 @@
+#include <iostream> 
+
+int main()
+{
+    std::cout << "Hello world!"; 
+
+    return 0;
+}
+
+/* 
+Can be read as: "Hello world!" is sent to character output 
+- cout stands for "character output" or "console output" 
+- << is the output operator (like an arrow showing the direction of data flow) 
+
+The text "Hello World!" is being sent to the character output stream, which in this case displays it in the console/terminal.
+*/
