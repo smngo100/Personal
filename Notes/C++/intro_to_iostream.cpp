987f936a17250d91b2519fc896f542febdc643a6
smngo100 <smngo100@gmail.com>
2025-01-09 21:23:26 -0800

    Update intro_to_iostream.cpp

--- Notes/C++/intro_to_iostream.cpp
+++ Notes/C++/intro_to_iostream.cpp
@@ -2,3 +2,11 @@ iostream // io stands for input/output
 
 std::cout  // cout stands for "character output" 
 
+// Concatenate (link together) multiple pieces of output
+#include <iostream> 
+
+int main() 
+{
+  std::cout << "Hello" << " world!"; 
+  return 0;
+}
