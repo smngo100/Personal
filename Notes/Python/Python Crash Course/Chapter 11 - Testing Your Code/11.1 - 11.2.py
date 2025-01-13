0a2249f283c7fc44a3efe705abfdab0a3e37f402
smngo100 <smngo100@gmail.com>
2025-01-13 00:15:47 -0800

    Update 11.1 - 11.2.py

--- Notes/Python/Python Crash Course/Chapter 11 - Testing Your Code/11.1 - 11.2.py
+++ Notes/Python/Python Crash Course/Chapter 11 - Testing Your Code/11.1 - 11.2.py
@@ -1,4 +1,5 @@
 # 11-1. City, Country 
+# Run test, and make sure test_city_country() passes
 city_functions.py 
 def get_formatted_city_country(city, country):
     city_country = f"{city}, {country}"
@@ -6,7 +7,6 @@ def get_formatted_city_country(city, country):
 
 test_cities.py 
 from city_functions import get_formatted_city_country
-
 def test_city_country():
     city_country =  get_formatted_city_country('santiago', 'chile')
     assert city_country == 'Santiago, Chile'
@@ -15,3 +15,38 @@ def test_city_country():
 
 
 # 11-2. Population 
+# Modify the function so that it requires a third parameter, population 
+# Run the test again, and make sure test_city_country() fails this time. 
+city_functions.py 
+def get_formatted_city_country(city, country, population):
+    city_country = f"{city.title()}, {country.title()} - population {population}"
+    return city_country
+
+test_cities.py 
+from city_functions import get_formatted_city_country
+def test_city_country():
+    city_country =  get_formatted_city_country('santiago', 'chile')
+    assert city_country == 'Santiago, Chile'
+
+
+# Modify the function so the population parameter is optional. Run the test, and make sure test_city_country() passes again.
+city_functions.py 
+def get_formatted_city_country(city, country, population=''):
+    if population:
+        city_country = f"{city.title()}, {country.title()} - population {population}"
+    else:
+        city_country = f"{city.title()}, {country.title()}"
+    return city_country
+
+test_cities.py 
+from city_functions import get_formatted_city_country
+
+def test_city_country():
+    city_country =  get_formatted_city_country('santiago', 'chile', 5000000)
+    assert city_country == 'Santiago, Chile - population 5000000'
+
+
+# Second test called test_city_country_population() verifies that the user can call the function with the specific values. Run the test, and make sure this new test passes.
+
+
+
