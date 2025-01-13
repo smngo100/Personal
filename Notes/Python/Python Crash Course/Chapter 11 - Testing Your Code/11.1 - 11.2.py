8edaae6de53679f296abf31727e6e026f750a424
smngo100 <smngo100@gmail.com>
2025-01-13 00:19:07 -0800

    Update 11.1 - 11.2.py

--- Notes/Python/Python Crash Course/Chapter 11 - Testing Your Code/11.1 - 11.2.py
+++ Notes/Python/Python Crash Course/Chapter 11 - Testing Your Code/11.1 - 11.2.py
@@ -47,6 +47,20 @@ def test_city_country():
 
 
 # Second test called test_city_country_population() verifies that the user can call the function with the specific values. Run the test, and make sure this new test passes.
+city_functions.py 
+def get_formatted_city_country(city, country, population=''):
+    if population:
+        city_country = f"{city.title()}, {country.title()} - population {population}"
+    else:
+        city_country = f"{city.title()}, {country.title()}"
+    return city_country
 
+test_cities.py 
+from city_functions import get_formatted_city_country
+def test_city_country():
+    city_country =  get_formatted_city_country('santiago', 'chile')
+    assert city_country == 'Santiago, Chile'
 
-
+def test_city_country_population():
+    city_country_population = get_formatted_city_country('santiago', 'chile', 5000000)
+    assert city_country_population == 'Santiago, Chile - population 5000000'
