d6a6404abed6f83828f8dfc3824297577ebce47b
smngo100 <smngo100@gmail.com>
2025-01-14 01:03:14 -0800

    Update file.py

--- Notes/Python/Python Crash Course/Chapter 10 - Files and Exceptions/file.py
+++ Notes/Python/Python Crash Course/Chapter 10 - Files and Exceptions/file.py
@@ -61,7 +61,7 @@ from pathlib import Path
 
 contents = "I love programming.\n"
 contents += "I love creating new games.\n"
-contents += "I also love working with data.n\"
+contents += "I also love working with data.\n"
 
 path = Path('programming.txt') 
 path.write_text(contents) 
