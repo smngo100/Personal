91e239127cc7ebf429f3fab7153af301757bd3c1
smngo100 <smngo100@gmail.com>
2025-01-15 23:23:32 -0800

    Update 11.3.py

--- Notes/Python/Python Crash Course/Chapter 11 - Testing Your Code/11.3.py
+++ Notes/Python/Python Crash Course/Chapter 11 - Testing Your Code/11.3.py
@@ -1,7 +1,7 @@
 # 11-3. Employee
-# Without using a fixture
-employee.py 
 
+##### Without using a fixture #####
+# employee.py 
 class Employee:
     def __init__(self, first_name, last_name, annual_salary):
         self.first_name = first_name
@@ -12,8 +12,7 @@ class Employee:
         self.annual_salary += raise_amount
 
 
-test_employee.py 
-
+# test_employee.py 
 from employee import Employee
 
 def test_give_default_raise():
@@ -22,9 +21,35 @@ def test_give_default_raise():
     assert employee.annual_salary == 65_000
 
 def test_give_custom_raise():
-    employee = Employee('Oatmeal', 'Raisin', 50_000)
+    employee = Employee('Oatmeal', 'Raisin', 60_000)
     employee.give_raise(10_000)
-    assert employee.annual_salary == 60_000
+    assert employee.annual_salary == 70_000
 
 
-# Using a fixture
+##### Using a fixture #####
+# employee.py
+class Employee:
+    def __init__(self, first_name, last_name, annual_salary):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.annual_salary = annual_salary
+
+    def give_raise(self, raise_amount=5_000):
+        self.annual_salary += raise_amount
+
+# test_employee.py 
+from employee import Employee
+import pytest
+
+@pytest.fixture
+def employee():
+    employee = Employee('Blueberry', 'Muffin', 60_000)
+    return employee
+
+def test_give_default_raise(employee):
+    employee.give_raise()
+    assert employee.annual_salary == 65_000
+
+def test_give_custom_raise(employee):
+    employee.give_raise(10_000)
+    assert employee.annual_salary == 70_000
