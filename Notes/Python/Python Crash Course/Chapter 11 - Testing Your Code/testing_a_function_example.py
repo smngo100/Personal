0831696076cba26a3d3e29a15a12cbe5502de914
smngo100 <smngo100@gmail.com>
2025-01-15 17:13:57 -0800

    Update testing_a_function_example.py

--- Notes/Python/Python Crash Course/Chapter 11 - Testing Your Code/testing_a_function_example.py
+++ Notes/Python/Python Crash Course/Chapter 11 - Testing Your Code/testing_a_function_example.py
@@ -3,3 +3,30 @@ def get_formatted_name(first, last):
     """Generate a neatly formatted full name."""
     full_name = f"{first} {last}"
     return full_name.title()
+
+    
+# Checking that the get_formatted_name() function works 
+names.py
+from name_function import get_formatted_name
+
+print("Enter 'q' at any time to quit.")
+while True:
+    first = input("\nPlease give me a first name: ")
+    if first == 'q':
+        break
+    last = input("Please give me a last name: ")
+    if last == 'q':
+        break
+
+    formatted_name = get_formatted_name(first, last)
+    print(f"\tNeatly formatted name: {formatted_name}.")
+
+
+# Testing the get_formatted_name() function
+test_name_function.py
+from name_function import get_formatted_name
+
+def test_first_last_name():
+    """Do names like 'Janis Joplin' work?"""
+    formatted_name = get_formatted_name('janis', 'joplin')
+    assert formatted_name == 'Janis Joplin'
