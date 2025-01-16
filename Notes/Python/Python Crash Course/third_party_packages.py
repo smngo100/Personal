8d1edcfab31fe2182ba152d674c77483b4dbf442
smngo100 <smngo100@gmail.com>
2025-01-16 00:12:11 -0800

    Create third_party_packages.py

---
+++ Notes/Python/Python Crash Course/third_party_packages.py
@@ -0,0 +1,23 @@
+# Documentation of syntax for installing Pyhotn third-party packages 
+
+##### Pip #####
+# Updating pip 
+$ python -m pip install --upgrade package_name  # might not need $
+
+
+##### Pytest #####
+# Installing pytest
+$ python -m pip install --user pytest  # might not need $  
+
+# Running Pytest 
+python -m pytest
+
+
+##### Pygame #####
+python -m pip install --user pygame
+
+
+##### GENERAL COMMAND FOR INSTALLING THIRD-PARTY PACKAGES #####
+$ python -m pip install --user package_name  # might not need $ 
+# or 
+pip install <python package> 
