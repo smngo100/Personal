8efe3a59b4ea73e6ffd9cbc944b6de04f98adde1
smngo100 <smngo100@gmail.com>
2025-01-15 23:26:34 -0800

    Update 11.3.py

--- Notes/Python/Python Crash Course/Chapter 11 - Testing Your Code/11.3.py
+++ Notes/Python/Python Crash Course/Chapter 11 - Testing Your Code/11.3.py
@@ -1,4 +1,11 @@
 # 11-3. Employee
+"""
+Write a class called Employee. The __init__() method should take in a first name, a last name, and an annual salary, and store each of these as attributes. 
+Write a method called give_raise() that adds $5,000 to the annual salary by default but also accepts a different raise amount.
+Write a test file for Employee with two test functions, test_give_default_raise() and test_give_custom_raise(). 
+Write your tests once without using a fixture, and make sure they both pass. 
+Then write a fixture so you don’t have to create a new employee instance in each test function. Run the tests again, and make sure both tests still pass.
+"""
 
 ##### Without using a fixture #####
 # employee.py 
