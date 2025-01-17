d212cd2be8a67252ac58e15c9e170920df0cc0e7
smngo100 <smngo100@gmail.com>
2025-01-16 22:07:32 -0800

    Create attribute_w_self.py

---
+++ Notes/Python/Python Crash Course/Chapter 09 - Classes/attribute_w_self.py
@@ -0,0 +1,4 @@
+"""
+When you create an attribute using self in a Python class, that attribute becomes available to all methods within that class. 
+This is because self refers to the instance of the class, and any attribute attached to it persist throughout the object's lifetime.
+"""
