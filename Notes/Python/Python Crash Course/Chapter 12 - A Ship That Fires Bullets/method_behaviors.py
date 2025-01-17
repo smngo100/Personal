49b6b1949fb8858f334cc6ccc15692bb76fb4952
smngo100 <smngo100@gmail.com>
2025-01-16 22:06:16 -0800

    Create method_behaviors.py

---
+++ Notes/Python/Python Crash Course/Chapter 12 - A Ship That Fires Bullets/method_behaviors.py
@@ -0,0 +1,2 @@
+pygame.init() # initializes the background settings that Pygame needs to work properly 
+pygame.display.set_mode() # creates a display window, on which we'll draw the game's graphical elements 
