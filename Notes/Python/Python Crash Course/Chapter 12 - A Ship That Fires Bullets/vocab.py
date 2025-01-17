72ba0c1deb29edee2e6f44e984f1f65563f7b766
smngo100 <smngo100@gmail.com>
2025-01-16 21:57:42 -0800

    Update and rename DELETELATER to vocab.py

---
+++ Notes/Python/Python Crash Course/Chapter 12 - A Ship That Fires Bullets/vocab.py
@@ -0,0 +1,3 @@
+# A 'SURFACE' in Pygame is a part of the screen where a game elemnt can be displayed.
+# An 'EVENT' is an action that the user performs while playing the game, such as pressing a key or moving the mouse. 
+
