68277f409225fe66d5930519b8f8e0bd6b4e87d4
smngo100 <smngo100@gmail.com>
2025-01-17 23:15:45 -0800

    Update vocab_n_method_behaviors.py

--- Notes/Python/Python Crash Course/Chapter 12 - A Ship That Fires Bullets/vocab_n_method_behaviors.py
+++ Notes/Python/Python Crash Course/Chapter 12 - A Ship That Fires Bullets/vocab_n_method_behaviors.py
@@ -2,6 +2,7 @@
 # A 'SURFACE' in Pygame is a part of the screen where a game elemnt can be displayed.
 # An 'EVENT' is an action that the user performs while playing the game, such as pressing a key or moving the mouse. 
 # A 'HELPER METHOD' does work inside a class but isn't meant to be used by code outside the class.
+           # In Python, a single leading underscore indiciates a helper method.
 
 
 ##### Method behaviors #####
