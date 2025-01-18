5e3d0228910d110c71e3c91412cce293547ac768
smngo100 <smngo100@gmail.com>
2025-01-18 00:10:15 -0800

    Update third_party_packages.py

--- Notes/Python/Python Crash Course/third_party_packages.py
+++ Notes/Python/Python Crash Course/third_party_packages.py
@@ -6,10 +6,10 @@ $ python -m pip install --upgrade <package_name>
 
 
 ##### Installing a third-party package #####
-$ python -m pip install --user package_name  
+$ python -m pip install --user <package_name>   
 # or 
-pip install package_name  
+pip install <package_name>   
 
 
 ##### Running a third-party package #####
-python -m package_name
+python -m <package_name> 
