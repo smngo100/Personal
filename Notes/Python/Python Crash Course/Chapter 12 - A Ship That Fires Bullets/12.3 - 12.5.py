122d1ea75fd0a02ae059b809fc866367c205e013
smngo100 <smngo100@gmail.com>
2025-01-22 00:01:25 -0800

    Create 12.3 - 12.5.py

---
+++ Notes/Python/Python Crash Course/Chapter 12 - A Ship That Fires Bullets/12.3 - 12.5.py
@@ -0,0 +1,18 @@
+# 12-3. Pygame Documentation 
+# The Pygame home page:
+https://www.pygame.org/news
+
+# The home page for the documentation:
+https://www.pygame.org/docs/
+
+
+# 12-4. Rocket 
+
+
+
+
+
+# 12-5. Keys 
+
+
+
