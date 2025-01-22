532584ef9395eb1177205cc120cf3d8bee6e7483
smngo100 <smngo100@gmail.com>
2025-01-21 23:56:59 -0800

    Update ship.py

--- Notes/Python/Python Crash Course/Chapter 12 - A Ship That Fires Bullets/ship.py
+++ Notes/Python/Python Crash Course/Chapter 12 - A Ship That Fires Bullets/ship.py
@@ -6,6 +6,7 @@ class Ship:
     def __init__(self, ai_game):
         """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # Load the ship image and get its rect.
@@ -15,6 +16,31 @@ class Ship:
         # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Store a float for the ship's exact horizontal position.
+        self.x = float(self.rect.x)
+
+        # Movement flag; start with a ship that's not moving.
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Update the ship's position based on the movement flag."""
+        # Update the ship's x value, not the rect.
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0: # Refer to the * below
+            self.x -= self.settings.ship_speed
+
+        # Update rect object from self.x.
+        self.rect.x = self.x
+
     def blitme(self):
         """Draw the ship at its current location."""
         self.screen.blit(self.image, self.rect)
+
+
+
+""" * : It is > 0 because...
+In Pygame, the origin (0, 0) is at the top-left corner of the screen, and coordinates increase as you go down and 
+to the right. On a 1200×800 screen, the origin is at the top-left corner, and the bottom-right corner has the 
+coordinates (1200, 800). These coordinates refer to the game window, not the physical screen."""
