e9c57059607f12716376fc0aad3ee9a3c994e6fa
smngo100 <smngo100@gmail.com>
2025-01-22 23:07:11 -0800

    Update vocab_n_method_behaviors.py

--- Notes/Python/Python Crash Course/Chapter 12 - A Ship That Fires Bullets/vocab_n_method_behaviors.py
+++ Notes/Python/Python Crash Course/Chapter 12 - A Ship That Fires Bullets/vocab_n_method_behaviors.py
@@ -12,6 +12,7 @@ run_game() # method contains a while loop that runs continually
            # the loop contains an event loop and code that manages screen updates
 pygame.event.get() # returns a list of events that have taken place since the last time this function was called
 pygame.display.flip() # tells Pygame to make the most rectnyl drawn screen visible
+get_rect() # returns a rectangle covering the entire surface
 
 
 ##### Events #####
