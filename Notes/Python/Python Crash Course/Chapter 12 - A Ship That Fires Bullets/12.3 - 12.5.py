0c2aeb31f3786782feb69dbc9c0fc100187c67f9
smngo100 <smngo100@gmail.com>
2025-01-25 00:34:44 -0800

    Update 12.3 - 12.5.py

--- Notes/Python/Python Crash Course/Chapter 12 - A Ship That Fires Bullets/12.3 - 12.5.py
+++ Notes/Python/Python Crash Course/Chapter 12 - A Ship That Fires Bullets/12.3 - 12.5.py
@@ -7,9 +7,130 @@ https://www.pygame.org/docs/
 
 
 # 12-4. Rocket 
+# rocket.py
+import sys
+import pygame
+from settings import Settings
+from ship import Ship
+
+class AlienInvasion:
+    def __init__(self):
+        pygame.init()
+        self.clock = pygame.time.Clock()
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screenheight = self.screen.get_rect().height
+        pygame.display.set_caption("Rocket")
+
+        self.ship = Ship(self)
+
+    def run_game(self):
+        while True:
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+            self.clock.tick(60)
+
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_UP:
+            self.ship.moving_up = True
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+        elif event.key == pygame.K_UP:
+            self.ship.moving_up = False
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = False
+
+    def _update_screen(self):
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        pygame.display.flip()
+
+if __name__ == '__main__':
+    ai = AlienInvasion()
+    ai.run_game()
 
 
+# ship.py 
+import pygame
 
+class Ship:
+    def __init__(self, ai_game):
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.screen_rect = ai_game.screen.get_rect()
+
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+
+        self.rect.center = self.screen_rect.center
+
+        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
+
+        self.moving_right = False
+        self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
+
+    def update(self):
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+        if self.moving_up and self.rect.top > 0:
+            self.y -= self.settings.ship_speed  # self.moving_up so why -=? Refer to * below
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.y += self.settings.ship_speed
+
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+    def blitme(self):
+        self.screen.blit(self.image, self.rect)
+
+# *: For self.moving_up it is -= where decreasing moves the object up
+# Why? Because remember that in computer graphics the origin (0, 0) is in the top-left corner of the screen.
+# Therefore, the y-values increase as they moves downward, and the y-values decrease as they upward.
+
+# Summary:
+    # Decreasing y (y -= value) moves the object up
+    # Increasing y (y += value) moves the object down
+    # > 0 means within the screen's top boundary
+    # < 0 would mean out of range (above the screen)
+
+# settings.py 
+class Settings:
+    def __init__(self):
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+
+        self.ship_speed = 1.5
 
 
 # 12-5. Keys 
