c9d1c219dc89a28506d3ca874f09060d52558bb9
smngo100 <smngo100@gmail.com>
2025-01-28 16:32:01 -0800

    Create uninitalized_variables.cpp

---
+++ Notes/C++/uninitalized_variables.cpp
@@ -0,0 +1,8 @@
+// Uninitialized variable: A variable that has not been given a known value (through initialization or assignment)
+
+// Note: 
+// Initialized = The object is given a known value at the point of definition. 
+// Assignment = The object is given a known value beyond the point of definition. 
+// Uninitialized = The object has not been given a known value yet. 
+
+
