333199d4e097b5c2fef31b99ff9b7c24ba9658b3
smngo100 <smngo100@gmail.com>
2025-01-29 10:54:15 -0800

    Update and rename uninitalized_variables.cpp to uninitalized_variables_n_undefined_behavior.cpp

--- Notes/C++/uninitalized_variables_n_undefined_behavior.cpp
+++ Notes/C++/uninitalized_variables_n_undefined_behavior.cpp
@@ -4,5 +4,3 @@
 // Initialized = The object is given a known value at the point of definition. 
 // Assignment = The object is given a known value beyond the point of definition. 
 // Uninitialized = The object has not been given a known value yet. 
-
-
