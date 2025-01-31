fa66550c09504b9d2dbfebe2a0db962051f7593d
smngo100 <smngo100@gmail.com>
2025-01-31 01:08:40 -0800

    Update keywords_n_naming_identifiers.cpp

--- Notes/C++/keywords_n_naming_identifiers.cpp
+++ Notes/C++/keywords_n_naming_identifiers.cpp
@@ -1,1 +1,17 @@
 // C++ reserves a set of 92 words for its own use. These words are called keywords (or reserved words). Each of these keywords has a special meaning within the C++ language.
+
+// Identifier naming rules 
+/* The identifier can not be a keyword. Keywords are reserved.
+The identifier can only be composed of letters (lower or upper case), numbers, and the underscore character. That means the name can not contain symbols (except the underscore) 
+nor whitespace (spaces or tabs). The identifier must begin with a letter (lower or upper case) or an underscore. It can not start with a number. 
+C++ is case sensitive, and thus distinguishes between lower and upper case letters. nvalue is different than nValue is different than NVALUE. */
+
+
+// Identifier naming best practices 
+// Convetional that variable names begin with a lowercase letter 
+// Often, function names start with a lowercase letter 
+// If variable or function is multi-word, there are 2 common conventions: 
+  // Words seprated underscores (snake_case) 
+  // Intercapped (camelCase, since the capital letters stick up like the humps on a camel)
+
+
