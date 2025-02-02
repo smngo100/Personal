aceb530535f5980cb363a9b8896b5249f5060e7a
smngo100 <smngo100@gmail.com>
2025-02-01 23:29:47 -0800

    Create whitespace_n_basic_formatting.cpp

---
+++ Notes/C++/whitespace_n_basic_formatting.cpp
@@ -0,0 +1,3 @@
+// Whitespace is a term that refers to characters that are used for formatting purposes. Typically refers to space, tabs, and newlines.
+// Generally used for 3 things: separating certain language elements, inside text, and for formatting code. 
+
