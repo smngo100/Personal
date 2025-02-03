fb462de7aac32a80e7059e17adf76158e4352a75
smngo100 <smngo100@gmail.com>
2025-02-03 00:28:18 -0800

    Update whitespace_n_basic_formatting.cpp

--- Notes/C++/whitespace_n_basic_formatting.cpp
+++ Notes/C++/whitespace_n_basic_formatting.cpp
@@ -1,3 +1,48 @@
 // Whitespace is a term that refers to characters that are used for formatting purposes. Typically refers to space, tabs, and newlines.
 // Generally used for 3 things: separating certain language elements, inside text, and for formatting code. 
 
+
+////////// Basic Formatting //////////
+// Recommendations for basic formatting 
+  // 1. Fine to use either tabs or spaces for identation. Recommended set the tabs to 4 spaces worth of indentation.
+  // 2. There are two conventional styles for function braces.
+    /* 
+    
+       int main() { 
+         // statements here 
+       }
+
+       OR 
+
+       int main() 
+       {
+         // statements here 
+       }
+       
+    */
+  // 3. Each statement within curly braces should start one tab in from the opening brace of the function it belongs to.
+  // 4. Lines should not be too long. Consider keeping your lines 80 characters or less in length.
+  // 5. If a long line is split with an operator (eg. << or +), the operator shuold be placed at the beginning of the next line, not the end of the current line.
+    /*
+      std::cout << 3 + 4
+          + 5 + 6
+          * 7 * 8;
+    */
+// 6. Use whitespace to make your code easier to read by aligning values or comments or adding spacing between blocks of code. 
+  /*
+    // Harder to read
+    cost = 57; 
+    pricePerItem = 24; 
+    value = 5; 
+    numberOfItems = 17;
+
+    // Easier to read
+    cost          = 57; 
+    pricePerItem  = 24; 
+    value         = 5; 
+    numberOfItems = 17;
+  */
+
+
+            
+  
