f14eec1c75204288fdb9d5ce3e11170293c1ce16
smngo100 <smngo100@gmail.com>
2025-02-03 20:22:39 -0800

    Update intro_to_literals_n_operators.cpp

--- Notes/C++/Chapter-1/intro_to_literals_n_operators.cpp
+++ Notes/C++/Chapter-1/intro_to_literals_n_operators.cpp
@@ -5,9 +5,9 @@
 
 ///// Literals and variables Similarities and Differences /////
 // Similar: Literals and variables both have a value (and a type) 
-// Difference: Variable's value can be set and changed through initialization and assignment respectively. The value of a literal is fixed and cannot be changed
-  // The literal 5 always has value 5
-  // This is why literals are called constants
+// Difference: Variable's value can be set and changed through initialization and assignment respectively. The value of a literal is fixed and cannot be changed.
+  // The literal 5 always has value 5.
+  // This is why literals are called constants.
 
 // Literal value can be printed directly. In the case of the variable, the value 5 must be fetched from the memory the variable represents. 
 // Explains why a literal is constant while a variable can be changed.
