1468b641b518e5073202c1c70732a3fce7e18f4a
smngo100 <smngo100@gmail.com>
2025-02-08 23:26:47 -0800

    Update intro_to_functions.cpp

--- Notes/C++/Chapter-2/intro_to_functions.cpp
+++ Notes/C++/Chapter-2/intro_to_functions.cpp
@@ -2,4 +2,6 @@
 
 // Function: A resusable sequence of statements designed to do a particular job. 
 // User-defined functions: Functions you write yourself.
-
+// A function call tells the CPU to interrupt the current function and execute another function. 
+  // The CPU essentialy "puts a bookmark" at the current point of execution, executes the functon named in the function call, 
+  // and then returns to the point it bookmarked and resumes execution.
