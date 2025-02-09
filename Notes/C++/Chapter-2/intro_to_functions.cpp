abf83ec2c08178f0d88d0d5a7ba7657348a9cd6b
smngo100 <smngo100@gmail.com>
2025-02-08 23:43:46 -0800

    Update intro_to_functions.cpp

--- Notes/C++/Chapter-2/intro_to_functions.cpp
+++ Notes/C++/Chapter-2/intro_to_functions.cpp
@@ -5,3 +5,11 @@
 // A function call tells the CPU to interrupt the current function and execute another function. 
   // The CPU essentialy "puts a bookmark" at the current point of execution, executes the functon named in the function call, 
   // and then returns to the point it bookmarked and resumes execution.
+
+////////// An example of a user-defined function ///// 
+returnType functionName() // This is the function header (tells the compiler about the existence of the function) 
+{
+  // THis is the function body (tells the compiler what the function does) 
+}
+
+// The first line is called the function header; tells the compiler about the existence of a function, function's name, and other info
