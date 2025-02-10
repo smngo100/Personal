8cef661a9086471f999506203846a505e0648a43
smngo100 <smngo100@gmail.com>
2025-02-09 16:37:20 -0800

    Update function_return_values.cpp

--- Notes/C++/Chapter-2/function_return_values.cpp
+++ Notes/C++/Chapter-2/function_return_values.cpp
@@ -1,2 +1,7 @@
 2.2 - Function return values (value-returning functions) 
 
+////////// Return Values //////////
+// To return a value back to the caller, 2 things are needed: 
+  // 1. Function has to indicate what type of value will be returned.
+    // Return type: The type that is defined before the function's name.
+  // 2. 
