b039596e65affe05eb03ccd5cd69464cca52dc8f
smngo100 <smngo100@gmail.com>
2025-02-09 16:45:30 -0800

    Update function_return_values.cpp

--- Notes/C++/Chapter-2/function_return_values.cpp
+++ Notes/C++/Chapter-2/function_return_values.cpp
@@ -4,4 +4,10 @@
 // To return a value back to the caller, 2 things are needed: 
   // 1. Function has to indicate what type of value will be returned.
     // Return type: The type that is defined before the function's name.
-  // 2. 
+  // 2. Inside the funciton that will return a value, we use a return statement to indicate the specific value being returned to the caller. 
+    // Consists of the return keyword, followed by an expression (aka return expression), ending with a semicolon.
+
+// The process of returning a copied value back to the caller is named return by value.
+
+
+////////// Fixing Our Challenge Program //////////
