f7e5d94cbbc29f807c0d22b62ec8346ebcff5ea8
smngo100 <smngo100@gmail.com>
2025-02-11 15:16:19 -0800

    Create void_functions.cpp

---
+++ Notes/C++/Chapter-2/void_functions.cpp
@@ -0,0 +1,10 @@
+2.3 - Void Functions (non-value returning functions) 
+
+///// Syntax for a Function Definition /////
+returnType identifier() // identifier replaced with the name of your function
+{ 
+// Your code here
+}
+
+////////// Void return values //////////
+// To tell the compiler that a function does not return a value, a return type of void is used.
