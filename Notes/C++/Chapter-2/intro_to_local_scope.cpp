cefc1e605fe93f531eff09a4f72d3aedc60e4e3a
smngo100 <smngo100@gmail.com>
2025-02-13 14:47:09 -0800

    Create intro_to_local_scope.cpp

---
+++ Notes/C++/Chapter-2/intro_to_local_scope.cpp
@@ -0,0 +1,27 @@
+2.5 - Introduction to local scope 
+
+////////// Local variables //////////
+// Local variables: Variables defined inside the body of a function. 
+
+
+////////// Local variable lifetime //////////
+// Local variable are destroyed in the opposite order of creation at the end of the set of curly braces in whcih it is defined.
+  // for a funciton parameter, at the end of the function
+// Object's lifetime is defined to be the time between its creation and destruction
+// Variable creation and destruction happen during runtime, not compile time 
+  // Lifetime is a runtime property
+
+// What happens when an object is destroyed/ Destroyed object becomes invalid.
+
+// Deallocated: Memory used by the object is freed up for reuse.
+
+
+////////// Local scope (block scope) //////////
+// An identifier's scope determines where the identifier can be seen and used within the source code. 
+  // In scope: When an identifier can be seen and used. 
+  // Out of scope: When an identifier cannot be seen and used.
+// Compile-time property
+// An identifier with local scope (aka block scope) is usable from the point of definition to the end of the innermost pair of... 
+  // ...curly braces containing the identifier (or for function parameters, at the end of the function). 
+// Ensures local variables cannot be used before point of deinition or after they're destroyed
+
