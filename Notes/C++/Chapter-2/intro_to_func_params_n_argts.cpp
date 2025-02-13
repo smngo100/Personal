d004aa9f075feafd3124b16becb46416d801785b
smngo100 <smngo100@gmail.com>
2025-02-13 00:17:30 -0800

    Create intro_to_func_params_n_argts.cpp

---
+++ Notes/C++/Chapter-2/intro_to_func_params_n_argts.cpp
@@ -0,0 +1,5 @@
+2.4 - Introduction to function parameters and arguments 
+
+////////// Function parameters and arguments //////////
+// Function parameters: A variable used in the header of a function.
+// Argument: A value that is passed from the caller to the function when a function call is made.
