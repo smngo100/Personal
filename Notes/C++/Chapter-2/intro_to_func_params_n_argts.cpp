9ec25d011b570eba5318eb48a6be2ff54e1731e6
smngo100 <smngo100@gmail.com>
2025-02-13 13:52:17 -0800

    Update intro_to_func_params_n_argts.cpp

--- Notes/C++/Chapter-2/intro_to_func_params_n_argts.cpp
+++ Notes/C++/Chapter-2/intro_to_func_params_n_argts.cpp
@@ -3,3 +3,9 @@
 ////////// Function parameters and arguments //////////
 // Function parameters: A variable used in the header of a function.
 // Argument: A value that is passed from the caller to the function when a function call is made.
+
+
+////////// Unreferenced parameters //////////
+// Unreferenced parameters: Functions that have parameters that are not uesd in the body of the funciton.
+// Unnamed parameter: A parameter without a name. 
+
