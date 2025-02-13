e01af0addbd62783c0bd3d1e6dcf812a357757d0
smngo100 <smngo100@gmail.com>
2025-02-13 14:04:13 -0800

    Update intro_to_func_params_n_argts.cpp

--- Notes/C++/Chapter-2/intro_to_func_params_n_argts.cpp
+++ Notes/C++/Chapter-2/intro_to_func_params_n_argts.cpp
@@ -8,4 +8,9 @@
 ////////// Unreferenced parameters //////////
 // Unreferenced parameters: Functions that have parameters that are not uesd in the body of the funciton.
 // Unnamed parameter: A parameter without a name. 
-
+  // Function parameter is optional.
+  // Suggests to use a comment to document what the unnamed parameter was.
+  // When a function parameter exists but is not used in the body of the function, do not give it a name. Can optionally put a name inside a comment.
+      void doSomething(int /*count*/)
+      {
+      }
