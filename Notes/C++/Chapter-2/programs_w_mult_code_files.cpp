6af4fa89c9d4fce1a2c757c018a0659e4c056d36
smngo100 <smngo100@gmail.com>
2025-02-15 16:35:46 -0800

    Update programs_w_mult_code_files.cpp

--- Notes/C++/Chapter-2/programs_w_mult_code_files.cpp
+++ Notes/C++/Chapter-2/programs_w_mult_code_files.cpp
@@ -1,3 +1,22 @@
 2.8 - Programs with multiple code files
 
-// nothing worth noting
+// Use function declaration when
+// Think of this as an import in Python 
+// In Python, you import a function from another file, but in C++ you do a function declaration
+
+// add.cpp: 
+int add(int x, int y) 
+{ 
+  return x + y; 
+}
+
+// main.cpp
+int add(int x, int y);  // function declaration
+
+#include <iostream> 
+int main() 
+{ 
+  std::cout << "The sum of 3 and 4 is: " << add(3, 4) << "\n";
+
+  return 0;
+}
