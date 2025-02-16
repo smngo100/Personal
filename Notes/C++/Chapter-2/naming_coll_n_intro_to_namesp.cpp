1d464b0d970e5bd302e2b4eaf3e1bf014732b452
smngo100 <smngo100@gmail.com>
2025-02-15 18:11:05 -0800

    Create naming_coll_n_intro_to_namesp.cpp

---
+++ Notes/C++/Chapter-2/naming_coll_n_intro_to_namesp.cpp
@@ -0,0 +1,10 @@
+2.9 - Naming collisions and an introduction to namespace 
+
+/* Naming collision (or naming conflict): If two identical identifiers are introduced into the same program 
+in a way that the compiler or linker can't tell them apart, the compiler or linker will produce an error. 
+*/
+
+////////// Scope regiosn //////////
+// SCOPE REGION: An area of source code where all declared identifiers are considered distinct from names declared in other scope.
+
+
