a39289a2a1a1eac3ab131ba87ccb3e48bfeda079
smngo100 <smngo100@gmail.com>
2025-02-15 23:40:11 -0800

    Update naming_coll_n_intro_to_namesp.cpp

--- Notes/C++/Chapter-2/naming_coll_n_intro_to_namesp.cpp
+++ Notes/C++/Chapter-2/naming_coll_n_intro_to_namesp.cpp
@@ -4,7 +4,10 @@
 in a way that the compiler or linker can't tell them apart, the compiler or linker will produce an error. 
 */
 
-////////// Scope regiosn //////////
+////////// Scope regions //////////
 // SCOPE REGION: An area of source code where all declared identifiers are considered distinct from names declared in other scope.
 
+////////// Namespaces //////////
+// NAMESPACE: Provides another type of scope region (called the namespace scope) that allows you to declare names inside of it for the purpose of dismbiguation.
 
+////////// The global namespace //////////
