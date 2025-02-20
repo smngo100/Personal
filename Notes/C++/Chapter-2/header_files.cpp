e894b65f26a040166161e08f20b77b158eec7efc
smngo100 <smngo100@gmail.com>
2025-02-19 22:16:09 -0800

    Update header_files.cpp

--- Notes/C++/Chapter-2/header_files.cpp
+++ Notes/C++/Chapter-2/header_files.cpp
@@ -16,3 +16,19 @@
 // If a header file is paired with a code file (e.g. add.h with add.cpp), they should both have the same base name (add)
 // To use the header file in main.cpp or any file, we have to #include (sing quotes, not angle brackets) 
 #include "add.h" 
+
+
+//////////////////// Source files should include their paired header ////////////////////
+// Source files should #include their paired header file (if one exists) 
+// Avoid #including .cpp files 
+
+
+//////////////////// Angled brackets vs double quotes ////////////////////
+// Angled brackets vs double quotes helps give the preprocessor a clue as to where it should look for header files.
+// Use angled brackets to include headers that come with your compiler, OS, or third-party libraries you've installed elsewhere on your system.
+// Use double quotes to include header files that you've written or are expected to be found in the current directory. 
+
+
+
+
+
