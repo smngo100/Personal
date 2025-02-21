92111c1dcb796d16282e3ac8f8f37bb0ff81bd71
smngo100 <smngo100@gmail.com>
2025-02-21 11:58:25 -0800

    Update header_guards.cpp

--- Notes/C++/Chapter-2/header_guards.cpp
+++ Notes/C++/Chapter-2/header_guards.cpp
@@ -1,3 +1,16 @@
 2.12 - Header guards
 
+//////////////////// Header guards ////////////////////
+// Header guard (aka include guard) are conditional compilation directives that take the following form: 
+#ifndef SOME_UNIQUE_NAME_HERE
+#define SOME_UNIQUE_NAME_HERE
 
+// your declarations (and certain types of definitions) here 
+
+#endif
+
+
+//////////////////// #pragma once ////////////////////
+#pragma once 
+// Serves the same purpose as header guards to avoid a header file from being included multiple times. 
+// We're requesting the compiler guard the header.
