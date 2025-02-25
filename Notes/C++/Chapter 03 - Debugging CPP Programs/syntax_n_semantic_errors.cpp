0d847f4cf78a36f1d5a5193251754a8f95e0cd23
smngo100 <smngo100@gmail.com>
2025-02-24 21:51:32 -0800

    Update syntax_n_semantic_errors.cpp

--- Notes/C++/Chapter 03 - Debugging CPP Programs/syntax_n_semantic_errors.cpp
+++ Notes/C++/Chapter 03 - Debugging CPP Programs/syntax_n_semantic_errors.cpp
@@ -1,8 +1,12 @@
 3,1 - Syntax and semantic errors 
 
+////////// Errors fall into 1 of 2 categories //////////
+// 1. Syntax errors 
+// 2. Semantic errors
+
 ////////// Syntax errors //////////
 // A syntax error occurs when you write a statement that is not valid according to the grammar of the C++ language.
 
-////////// Semantic errors //////////
+////////// Semantic errors (logic errors) //////////
 // A semantic error is an error in meaning.
 // Occur when a statement is syntactically valid, but either violates other rules of the language, or does not do what the programmer intended.
