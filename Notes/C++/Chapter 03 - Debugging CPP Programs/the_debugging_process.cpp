dcb5d390842766ccbc63b414189166dd653c7921
smngo100 <smngo100@gmail.com>
2025-02-25 14:32:41 -0800

    Update and rename the_debugging_process.cp to the_debugging_process.cpp

---
+++ Notes/C++/Chapter 03 - Debugging CPP Programs/the_debugging_process.cpp
@@ -0,0 +1,19 @@
+3.2 - The debugging process
+
+
+//////////////////// A general approach to debugging ////////////////////
+// 6 steps 
+  // 1. Find the root cause of the problem.
+  // 2. Understnd why the issue is occurring.
+  // 3. Determine how you'll fix the issue.
+  // 4. Repair the issue causing the problem.
+  // 5. Retest to ensure the problem has been fixed.
+  // 6. Retest to ensure no new problems have emerged.
+
+
+//////////////////// SUMMARY ////////////////////
+// 1. Find the root cause. 
+// 2. Understand the problem.
+// 3. Determine a fix. 
+// 4. Repair the issue. 
+// 5. Retest
