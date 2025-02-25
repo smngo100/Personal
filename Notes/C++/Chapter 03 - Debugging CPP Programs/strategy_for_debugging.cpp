88aa573ac7ff61b65e8a91ea2bc21d8cfde00146
smngo100 <smngo100@gmail.com>
2025-02-25 14:50:19 -0800

    Update strategy_for_debugging.cpp

--- Notes/C++/Chapter 03 - Debugging CPP Programs/strategy_for_debugging.cpp
+++ Notes/C++/Chapter 03 - Debugging CPP Programs/strategy_for_debugging.cpp
@@ -1,4 +1,11 @@
 3.3 - A strategy for debugging 
 
 //////////////////// Finding problems via code inspection ////////////////////
-//
+// Approach
+  // 1. Figure out how to reproduce the problem. 
+  // 2. Run the program and gather information to narrow down where the problem is. 
+  // 3. Repeat the prior steup until you find the problem.
+
+
+//////////////////// Reproducing the problem ////////////////////
+// Reproduction steps are a list of clera and precise steps that can be followed to cause an issue to recur with a high level of predictability. 
