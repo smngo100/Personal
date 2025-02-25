31ed8db544366e3025119ec9249cedf0f8fd73d8
smngo100 <smngo100@gmail.com>
2025-02-25 15:30:48 -0800

    Update basic_debugging_tactics.cpp

--- Notes/C++/Chapter 03 - Debugging CPP Programs/basic_debugging_tactics.cpp
+++ Notes/C++/Chapter 03 - Debugging CPP Programs/basic_debugging_tactics.cpp
@@ -2,4 +2,9 @@
 
 
 //////////////////// Basic debugging tactics ////////////////////
-// 
+// Debugging tactic #1: Commenting out your code 
+// Debugging tactic #2: Validating your code flow 
+// Debugging tactic #3: Printing values
+
+
+// Tip: Use std::cerr instead of std::cout when debugging bc it's unbuffered
