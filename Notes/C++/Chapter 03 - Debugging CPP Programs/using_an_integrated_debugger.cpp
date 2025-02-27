bca60e6a7ce218d6e75732694dd3c47b87e20339
smngo100 <smngo100@gmail.com>
2025-02-26 22:35:46 -0800

    Update using_an_integrated_debugger.cpp

--- Notes/C++/Chapter 03 - Debugging CPP Programs/using_an_integrated_debugger.cpp
+++ Notes/C++/Chapter 03 - Debugging CPP Programs/using_an_integrated_debugger.cpp
@@ -1,2 +1,8 @@
 3.6 - Using an integrated debugger: Stepping 
 
+// Program state (aka state): Tracked information
+
+
+//////////////////// The debugger ////////////////////
+// A debugger is a computer program that allows the programmer to control how another program executes and examine the program state while that program is running.
+  
