ba4a219175c976ddd51902e4c7d47dc18b5aa752
smngo100 <smngo100@gmail.com>
2025-02-28 01:00:28 -0800

    Update using_an_integrated_debugger.cpp

--- Notes/C++/Chapter 03 - Debugging CPP Programs/using_an_integrated_debugger.cpp
+++ Notes/C++/Chapter 03 - Debugging CPP Programs/using_an_integrated_debugger.cpp
@@ -6,3 +6,10 @@
 //////////////////// The debugger ////////////////////
 // A debugger is a computer program that allows the programmer to control how another program executes and examine the program state while that program is running.
   
+//////////////////// Stepping ////////////////////
+// Stepping is the name for a set of related debugger features that let us execute (step through) our code statemeny by statement.
+
+//////////////////// Step into ////////////////////
+// Step into command executes next statement, if next statement contains function call, step into causes the program to jump to the top of the funciton being called, where it will pause.
+
+  
