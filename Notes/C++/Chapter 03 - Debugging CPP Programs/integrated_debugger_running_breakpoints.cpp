d30b451791d68b9a3fb90af92d323d5b48c22ee5
smngo100 <smngo100@gmail.com>
2025-03-01 14:39:41 -0800

    Create integrated_debugger_running_breakpoints.cpp

---
+++ Notes/C++/Chapter 03 - Debugging CPP Programs/integrated_debugger_running_breakpoints.cpp
@@ -0,0 +1,16 @@
+3.7 - Using an integrated debugger: Running and breakpoints
+
+//////////////////// Run to cursor ////////////////////
+// Run to cursor command executes the program until execution reaches the statement selected by your cursor
+// It returns control to you so you can debug starting at that point
+
+//////////////////// Continue ////////////////////
+/* The continue debug command simply continues running the program as per normal, 
+either until the program terminates, or until something triggers control to return back to you again */
+
+//////////////////// Start ////////////////////
+// The start command performs the same action as continue, just starting from the beginning of the program
+
+//////////////////// Breakpoints ////////////////////
+// A breakpoint is a special marker that tells the debugger to stop execution of the program at the breakpoint when running in debug mode
+
