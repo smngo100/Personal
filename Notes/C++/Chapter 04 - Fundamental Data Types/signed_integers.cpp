47cd88ac41a55c42ae80d9e1ec8bb34fc494d2cf
smngo100 <smngo100@gmail.com>
2025-03-02 14:17:39 -0800

    Create signed_integers.cpp

---
+++ Notes/C++/Chapter 04 - Fundamental Data Types/signed_integers.cpp
@@ -0,0 +1,14 @@
+4.4 - Signed integers 
+
+// An integer is an integral type that can represent positive and negative whole numbers, including 0.
+
+//////////////////// Signed integers ////////////////////
+// The attribute of being positive, negative, or zero is called the number's sign.
+// By default, integers in C++ are signed, which means the number's sign is stored as part of the value. 
+
+
+//////////////////// Defining signed integers ////////////////////
+// Prefer the shorthand types that do not use the int suffix or signed prefix 
+
+//////////////////// Signed integer ranges ////////////////////
+//
