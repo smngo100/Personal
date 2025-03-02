0de1c7d3f9cf5f815933e4c77fde2d43249cb1c7
smngo100 <smngo100@gmail.com>
2025-03-02 13:41:09 -0800

    Update intro_fund_data_type.cpp

--- Notes/C++/Chapter 04 - Fundamental Data Types/intro_fund_data_type.cpp
+++ Notes/C++/Chapter 04 - Fundamental Data Types/intro_fund_data_type.cpp
@@ -1,1 +1,21 @@
 4.1 - Introduction to fundamental data types
+
+// The smallest unit of memory is a binary digit (aka bit), which can hold a value of 0 or 1. 
+// Memory is organized into sequential units called memory addresses (or addresses) 
+// A byte is a group of bits that are operated on as a unit. 
+// 1 byte = 8 bits 
+
+
+//////////////////// Data types ////////////////////
+// Data type (aka type) tells the compiler how to interpret the contents of memory in some meaningful way.
+
+//////////////////// Fundamental data types ////////////////////
+// Fundamental data types (aka basic types or primitive types) are predefined data types
+
+//////////////////// Integer vs integral types ////////////////////
+// The standard integer types are short, int, long, long long 
+// The integral types are bool, the various char types, and the standard integer types 
+
+//////////////////// The _t suffix ////////////////////
+// e.g. std::nullptr_t use a _t suffix, which means "type"
+// applied to modern types, but many types don't have it, so it's not consistently applied
