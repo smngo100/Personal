74bcec6df3af25daf47f0c8588ef23e8aa972254
smngo100 <smngo100@gmail.com>
2025-03-02 14:09:34 -0800

    Create object_sizes_sizeof_operator.cpp

---
+++ Notes/C++/Chapter 04 - Fundamental Data Types/object_sizes_sizeof_operator.cpp
@@ -0,0 +1,4 @@
+4.3 - Object sizes and the sizeof oeprator
+
+//////////////////// The sizeof operator ////////////////////
+// The sizeof operator is a unary operator that takes either a type or a variable, and returns the size of an object of that type (in bytes). 
