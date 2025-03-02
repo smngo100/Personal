7db6749910d643ab22519ccf4209edae21aef9ec
smngo100 <smngo100@gmail.com>
2025-03-02 13:45:39 -0800

    Create void.cpp

---
+++ Notes/C++/Chapter 04 - Fundamental Data Types/void.cpp
@@ -0,0 +1,8 @@
+4.2 - Void 
+
+void // means "no type"
+// An incomplete type 
+// An incomplete type is a type that has been declared but not yet defined
+  
+//////////////////// Functions that do not return a value ////////////////////
+void // used to indicate that a function does not return a value
