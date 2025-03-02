465b692fd660a09bebad396dab2478a6524866dd
smngo100 <smngo100@gmail.com>
2025-03-02 14:18:11 -0800

    Update object_sizes_sizeof_operator.cpp

--- Notes/C++/Chapter 04 - Fundamental Data Types/object_sizes_sizeof_operator.cpp
+++ Notes/C++/Chapter 04 - Fundamental Data Types/object_sizes_sizeof_operator.cpp
@@ -1,4 +1,6 @@
 4.3 - Object sizes and the sizeof oeprator
 
+// A variable with n bits can hold 2^n possible values
+
 //////////////////// The sizeof operator ////////////////////
 // The sizeof operator is a unary operator that takes either a type or a variable, and returns the size of an object of that type (in bytes). 
