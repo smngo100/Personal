ad099363961241a1b0c7b1826515b020ea2456f9
smngo100 <smngo100@gmail.com>
2025-03-03 22:10:15 -0800

    Update unsigned_int_why_to_avoid.cpp

--- Notes/C++/Chapter 04 - Fundamental Data Types/unsigned_int_why_to_avoid.cpp
+++ Notes/C++/Chapter 04 - Fundamental Data Types/unsigned_int_why_to_avoid.cpp
@@ -1,2 +1,11 @@
 4.5 - Unsigned integers, and why to avoid them
 
+// Unsigned integers are integers that hold non-negative whole numbners.
+
+//////////////////// Unsigned integer range ////////////////////
+// An n-bit unsigned variable has a range of 0 to (2^n) - 1
+
+//////////////////// UThe controversy over unsigned numbers ////////////////////
+// Why avoid? Because of 2 behaviros that cause problems: 
+  // 1) Much easier to overflow the bottom of the range, bc the bottom of the range is 0
+  // 2) Unexpected behavior can result when you mix signed and unsigend integers
