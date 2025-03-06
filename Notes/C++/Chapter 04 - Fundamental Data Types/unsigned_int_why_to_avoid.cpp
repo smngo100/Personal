965fa978693733f80e5e4c0f50a3da4e7160ca91
smngo100 <smngo100@gmail.com>
2025-03-05 22:13:05 -0800

    Update unsigned_int_why_to_avoid.cpp

--- Notes/C++/Chapter 04 - Fundamental Data Types/unsigned_int_why_to_avoid.cpp
+++ Notes/C++/Chapter 04 - Fundamental Data Types/unsigned_int_why_to_avoid.cpp
@@ -6,6 +6,6 @@
 // An n-bit unsigned variable has a range of 0 to (2^n) - 1
 
 //////////////////// The controversy over unsigned numbers ////////////////////
-// Why avoid? Because of 2 behaviros that cause problems: 
+// Why avoid? Because of 2 behaviors that cause problems: 
   // 1) Much easier to overflow the bottom of the range, bc the bottom of the range is 0
   // 2) Unexpected behavior can result when you mix signed and unsigend integers
