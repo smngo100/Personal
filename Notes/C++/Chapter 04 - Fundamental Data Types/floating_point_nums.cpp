214a51debd46c256fbac57f6cda8ed882f642829
smngo100 <smngo100@gmail.com>
2025-03-08 21:01:49 -0800

    Update floating_point_nums.cpp

--- Notes/C++/Chapter 04 - Fundamental Data Types/floating_point_nums.cpp
+++ Notes/C++/Chapter 04 - Fundamental Data Types/floating_point_nums.cpp
@@ -1,7 +1,7 @@
 4.8 - Floating point numbers 
 
 // A floating point type variable is a variable that can hold a number with a fractional component.
-// Floating part of the name refers to the fact that the decmal point can "float", ala support a variable number of digits before and after the decimal point.
+// Floating part of the name refers to the fact that the decmal point can "float", it can support a variable number of digits before and after the decimal point.
 // Floating point data types are always signed (can hold positive and negative values)
 
 //////////////////// C++ floating point types ////////////////////
@@ -11,4 +11,3 @@
   // 3) extended-precision  long double
 
 // Actual size of these types are not defined 
-
