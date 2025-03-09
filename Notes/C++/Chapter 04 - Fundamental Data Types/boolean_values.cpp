3637273af0688e6dfd3d6ed38beeff367736fd0c
smngo100 <smngo100@gmail.com>
2025-03-09 16:14:04 -0700

    Create boolean_values.cpp

---
+++ Notes/C++/Chapter 04 - Fundamental Data Types/boolean_values.cpp
@@ -0,0 +1,12 @@
+4.9 - Boolean values 
+
+//////////////////// Boolean variables //////////////////// 
+// Boolean variables are variables that can have only two possible values: "true" and "false" 
+// Boolean values store integral values, they are considered an integral type 
+  // true is stored as integer 1
+  // false is stored as integer 0 
+
+
+//////////////////// Use std::boolalpha to print true or false //////////////////// 
+std::boolalpha // the output 0 to false and 1 to true 
+std::noboolalpha // turns it off 
