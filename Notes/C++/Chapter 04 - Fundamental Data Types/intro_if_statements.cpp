ff1bf75ace22c3b1ce6ba88c3bcadf56ce3a7141
smngo100 <smngo100@gmail.com>
2025-03-12 11:57:07 -0700

    Create intro_if_statements.cpp

---
+++ Notes/C++/Chapter 04 - Fundamental Data Types/intro_if_statements.cpp
@@ -0,0 +1,27 @@
+4.10 - Introduction to if statements 
+
+
+// An if statement allows us to execute one (or more) lines of code only if some condition is true
+  // Simplest kind of conditional statement 
+
+if (condition) { 
+  true_statement;
+}
+// A condition (aka conditional expression) is an expression that evaluates to a Boolean value
+
+
+//////////////////// If-else //////////////////// 
+if (condition { 
+  true_statement;
+} else { 
+  false_statement;
+}
+
+
+//////////////////// Non-Boolean conditionals //////////////////// 
+// Non-zero values get converted to Boolean true 
+// Zero-values get converted to Boolean false 
+
+
+//////////////////// If-statements and early returns //////////////////// 
+// Early return: A return-statement that is not the last statement in a function
