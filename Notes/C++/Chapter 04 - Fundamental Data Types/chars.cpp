b1c0593a296d040e4d183f97f708f6435670e491
smngo100 <smngo100@gmail.com>
2025-03-17 22:40:59 -0700

    Create chars.cpp

---
+++ Notes/C++/Chapter 04 - Fundamental Data Types/chars.cpp
@@ -0,0 +1,11 @@
+4.11 - Chars 
+
+
+// The char data type was designed to hold a single character
+// A character can be a single letter, number, symbol, or whitespace
+// ASCII stands for American Code for Information Interchange 
+// Character literals are always placed between single quotes (e.g. 'g', '1', '')
+
+// Initializing chars 
+char ch2{ 'a' };   // preferred
+char ch1{ 97 };    // not preferred
