dab165314598f7e47cada93fc375b046c51096aa
smngo100 <smngo100@gmail.com>
2025-03-18 11:03:19 -0700

    Update intro_type_cvr_static_cast.cpp

--- Notes/C++/Chapter 04 - Fundamental Data Types/intro_type_cvr_static_cast.cpp
+++ Notes/C++/Chapter 04 - Fundamental Data Types/intro_type_cvr_static_cast.cpp
@@ -6,4 +6,4 @@
 
 //////////////////// Explicit type conversion ////////////////////
 // Explicit type conversion: Allow the programmar to explicitly tell the compiler to convert a value from oen type to another type, and that we take full responsbility for the result of that conversion.
-static_case<new_type>(expression) 
+static_cast<new_type>(expression) 
