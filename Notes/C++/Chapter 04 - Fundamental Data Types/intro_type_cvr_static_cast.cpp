1e07ad0566669e76e0d90fd62b18226ff978c0d1
smngo100 <smngo100@gmail.com>
2025-03-17 22:51:52 -0700

    Create intro_type_cvr_static_cast.cpp

---
+++ Notes/C++/Chapter 04 - Fundamental Data Types/intro_type_cvr_static_cast.cpp
@@ -0,0 +1,9 @@
+4.12 - Introduction to type conversion and static_cast
+
+// Type conversion: The process of converting data from one type to another type.
+// Implicit type conversion: When the compiler does type conversion on our behalf without us explicitly asking.
+
+
+//////////////////// Explicit type conversion ////////////////////
+// Explicit type conversion: Allow the programmar to explicitly tell the compiler to convert a value from oen type to another type, and that we take full responsbility for the result of that conversion.
+static_case<new_type>(expression) 
