e7db2880ff7204130ad21f7368073f2d78ec15be
smngo100 <smngo100@gmail.com>
2025-03-19 22:20:23 -0700

    Create literals.cpp

---
+++ Notes/C++/Chapter 05 - Constants and Strings/literals.cpp
@@ -0,0 +1,6 @@
+5.2 - Literals 
+
+// Literals are values that are inserted directly into the code
+  // Aka literal constants because their meaning cannot be reddefined 
+// The type of a literal is deduced from the literal's value (e.g. 5 deducted to be of type int)
+  
