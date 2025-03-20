1901c641ff3543ba757d10853f45af0700ab0616
smngo100 <smngo100@gmail.com>
2025-03-19 22:03:17 -0700

    Update const_var.cpp

--- Notes/C++/Chapter 05 - Constants and Strings/const_var.cpp
+++ Notes/C++/Chapter 05 - Constants and Strings/const_var.cpp
@@ -1,3 +1,16 @@
 5.1 - Constant variables (named constants) 
 
 // A constant is a value that cannot be change during a program's execution.
+
+// 2 kinds of constants: 
+  // 1) Name constants are constant values that are assocaited with an identifier. Also called symbolic constants. 
+  // 2) Literal constants are constant values that are not associated with an identifier.
+
+// Constant variable: A variable whose value cannot be changed after initialization.
+
+///// Declaring a constant variable /////
+const double gravity { 9.8 }; // preferred use of const before type 
+
+// Place const before the type 
+// Don't use const for value parameters
+// Don't use const when returning by value
