4350d5dd76a87bd0f96ccc78ca19bdb85c1c77d4
smngo100 <smngo100@gmail.com>
2025-03-20 17:15:47 -0700

    Update literals.cpp

--- Notes/C++/Chapter 05 - Constants and Strings/literals.cpp
+++ Notes/C++/Chapter 05 - Constants and Strings/literals.cpp
@@ -4,3 +4,5 @@
   // Aka literal constants because their meaning cannot be reddefined 
 // The type of a literal is deduced from the literal's value (e.g. 5 deducted to be of type int)
   
+// A string is a collection of sequential characters used to represent text (such as names, words, and sentences) 
+// Avoid magic numbers
